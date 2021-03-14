da602b7be2c20fed8f77bee9268ef0f023195747
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-03-14 14:28:34 +0530

    Time: 132 ms (56.02%), Space: 15.1 MB (75.20%) - LeetHub

---
+++ set-matrix-zeroes/set-matrix-zeroes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        sizeR = len(matrix)
+        sizeC = len(matrix[0])
+        setR = set()
+        setC = set()
+        
+        for i in range(sizeR):
+            for j in range(sizeC):
+                if matrix[i][j] == 0:
+                    setR.add(i)
+                    setC.add(j)
+        for i in range(sizeR):
+            for j in range(sizeC):
+                if i in setR:
+                    matrix[i][j] = 0
+                elif j in setC:
+                    matrix[i][j] = 0
+        #print(setR,setC,matrix)
+                    
+        
