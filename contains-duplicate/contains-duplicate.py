474d3ef477f995fa0ae89e2bed436ee9a1c2a31f
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-04-11 16:01:49 +0530

    Time: 116 ms (76.11%), Space: 21.6 MB (14.44%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashMap = {}
+        
+        for i in nums:   
+            if i not in hashMap:
+                hashMap[i] = 1
+            else:
+                return True
+        
+        return False
