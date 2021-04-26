092968e74a4a24d7cb010951d615b102d92022b3
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-04-26 08:51:29 -0700

    Time: 56 ms (59.83%), Space: 15.1 MB (14.39%) - LeetHub

---
+++ increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -0,0 +1,28 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        start = 0 
+        left = 0
+        right = 0
+        size = len(nums)
+        min1 = min2 = 2**31 - 1
+        def bruteForce(size,nums):
+            for i in range(size):
+                for j in range(i+1,size):
+                    for k in range(j+1,size):
+                        if nums[i] < nums[j] < nums[k]:
+                            return True
+            return False
+    
+        for i,val in enumerate(nums):
+            if val<min1:
+                min1 = val
+                start = i
+            if min1<val<min2 and start<i:
+                min2 = val
+                left = i
+            if min1<min2<val and left<i:
+                return True
+            
+        return False
+                
+                        
