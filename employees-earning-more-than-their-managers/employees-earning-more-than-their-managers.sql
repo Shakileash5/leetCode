4e0254b32270e475cb581bf9e41e89ea51bfee10
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-19 00:22:58 +0530

    Time: 409 ms (30.43%), Space: 0B (100.00%) - LeetHub

--- employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
 #select name from (select Id,name,salary,managerId from Employee where managerId is not Null) as worker
 #where worker.salary > (SELECT salary FROM Employee LIMIT managerId,1) 
-select worker.name as Employee from (select Id,name,salary,managerId from Employee) as worker join (select id,name,salary from Employee ) as manager on worker.managerid = manager.id where worker.salary > manager.salary;
+#select worker.name as Employee from (select Id,name,salary,managerId from Employee) as worker join (select id,name,salary from Employee ) as manager on worker.managerid = manager.id where worker.salary > manager.salary;
+
+select e.name as 'Employee' from (select id,name,managerId,salary from Employee) as e join
+    (select id,name,salary from employee) as m on e.managerId = m.id
+    where e.salary>m.salary;
