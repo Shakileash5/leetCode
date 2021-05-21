1217238be284fd6fc44b82ca62012aadb7ca8b08
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-21 13:13:55 +0530

    Time: 276 ms (98.47%), Space: 0B (100.00%) - LeetHub

--- rising-temperature/rising-temperature.sql
+++ rising-temperature/rising-temperature.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-Select Distinct W2.id from Weather W1 , Weather W2 where W1.recordDate + INTERVAL 1 DAY = W2.recordDate  and W1.Temperature < W2.Temperature;
+#Select Distinct W2.id from Weather W1 , Weather W2 where W1.recordDate + INTERVAL 1 DAY = W2.recordDate  and W1.Temperature < W2.Temperature;
+
+select w2.id from weather w2 inner join weather w1 on w2.recordDate = w1.recordDate+ interval 1 DAY and w2.temperature>w1.temperature; 
