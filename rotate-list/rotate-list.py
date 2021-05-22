328619157109afb68754b5c269071dec659f7d95
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-22 20:15:47 +0530

    Time: 64 ms (6.26%), Space: 14.4 MB (28.75%) - LeetHub

--- rotate-list/rotate-list.py
+++ rotate-list/rotate-list.py
@@ -4,6 +4,37 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def rotateRightSolTwo(self,head,n):
+        prev = None
+        tempHead = head
+        nthNode = head
+        endNode = None
+        count = 0
+        size = 0
+        while head!= None:
+            size+=1
+            head = head.next
+        head = tempHead
+        if size == 0:
+            return head
+        n = n%size
+        if n == 0 or size == 0:
+            return head
+        
+        while head!=None:
+            if count>=n:
+                prev = nthNode
+                nthNode = nthNode.next
+            count+=1
+            endNode = head
+            head = head.next
+        #print(prev.val,nthNode.val)
+        prev.next = None
+        endNode.next = tempHead
+        return nthNode
+            
+    
+    
     def rotateRight(self, head: ListNode, k: int) -> ListNode:
         proxyHead = head
         headC = head
@@ -12,40 +43,42 @@ class Solution:
         flag = 0
         headL = head
         
-        if head == None:
-            return head
-        def length(size,curr):
-            if curr.next==None:
-                return size
-            return length(size+1,curr.next)
-            
-        size = length(1,headL)
-        if size<k:
-            k = k%size
-        if size == k or k == 0:
-            return head
+        def naiveSolution():
+            if head == None:
+                return head
+            def length(size,curr):
+                if curr.next==None:
+                    return size
+                return length(size+1,curr.next)
+
+            size = length(1,headL)
+            if size<k:
+                k = k%size
+            if size == k or k == 0:
+                return head
+
+
+            while head.next!=None:
+                if count>=k:
+                    flag = 1
+                    if breakPoint.next == None:
+                        breakPoint = headC
+                    else:
+                        breakPoint = breakPoint.next
+
+                #if flag == 0 and head.next.next == None:
+                #prev = head
+                head = head.next
+                count+=1
+                #print(count,breakPoint,"\n",head)
+
+            print(breakPoint,proxyHead)
+            proxyHead = breakPoint.next
+            print("\n",proxyHead)
+            head.next = headC
+            breakPoint.next = None
         
-            
-        while head.next!=None:
-            if count>=k:
-                flag = 1
-                if breakPoint.next == None:
-                    breakPoint = headC
-                else:
-                    breakPoint = breakPoint.next
-            
-            #if flag == 0 and head.next.next == None:
-            #prev = head
-            head = head.next
-            count+=1
-            #print(count,breakPoint,"\n",head)
         
-        print(breakPoint,proxyHead)
-        proxyHead = breakPoint.next
-        print("\n",proxyHead)
-        head.next = headC
-        breakPoint.next = None
-        
-        return proxyHead
-                
+        #return proxyHead
+        return self.rotateRightSolTwo(head,k)        
         
