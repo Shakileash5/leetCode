826b67b2d72cbc9e1312d08b4a951e2846a7741a
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-23 21:40:02 +0530

    Time: 96 ms (75.32%), Space: 15.6 MB (62.82%) - LeetHub

--- minimum-path-sum/minimum-path-sum.py
+++ minimum-path-sum/minimum-path-sum.py
@@ -3,8 +3,9 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
         
-        res = [[0 for j in range(m)] for i in range(n)]
         
+        """
+        res = [[0 for j in range(m)] for i in range(n)]
         res[0][0] = grid[0][0]
         
         for j in range(1,m):
@@ -19,6 +20,18 @@ class Solution:
             for j in range(1,m):
                 res[i][j] = min(res[i-1][j],res[i][j-1]) + grid[i][j]
         
-        #print(res)
-        return res[-1][-1]
+        #print(res)"""
+        path = [[0 for j in range(m)] for i in range(n)]
+        path[0][0] = grid[0][0]
+        for i in range(1,n):
+            path[i][0] = path[i-1][0] + grid[i][0]
+        for j in range(1,m):
+            path[0][j] = path[0][j-1] + grid[0][j]
         
+        for i in range(1,n):
+            for j in range(1,m):
+                path[i][j] = min(path[i][j-1],path[i-1][j]) + grid[i][j]
+        
+        #print(path)
+        #return res[-1][-1]
+        return path[-1][-1]
