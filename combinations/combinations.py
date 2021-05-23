fa6ad783a2b0eb07da7eb94c7d27ba75747aabb0
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-23 22:53:09 +0530

    Time: 780 ms (6.46%), Space: 15.5 MB (97.78%) - LeetHub

--- combinations/combinations.py
+++ combinations/combinations.py
@@ -1,4 +1,14 @@
 class Solution:
+    
+    def combinations(self,idx,temp,res,n,k):
+        if len(temp) == k:
+            if temp not in res:
+                res.append(temp)
+            return
+        for i in range(idx,n+1):
+            self.combinations(i+1,temp+[i],res,n,k)
+        return
+    
     def combine(self, n: int, k: int) -> List[List[int]]:
         
         def backtrack(idx,temp,res):
@@ -11,7 +21,9 @@ class Solution:
                     backtrack(i,temp+[i],res)
             
             return 
+        
         res = []
-        backtrack(1,[],res)
+        #backtrack(1,[],res)
+        self.combinations(1,[],res,n,k)
         return res
         
