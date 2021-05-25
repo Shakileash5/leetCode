aae2e636b89d2e6e41e45ad2dca0fb8202de5bfb
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-25 17:21:47 +0530

    Time: 28 ms (82.84%), Space: 14.3 MB (70.71%) - LeetHub

--- reverse-linked-list-ii/reverse-linked-list-ii.py
+++ reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -4,36 +4,97 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+    def reverseBetweenTwo(self,head,left,right):
         if left == right:
             return head
         
-        currentNode = head
-        start = head
-        currentPosition = 1
+        tempHead = head
+        start = None
+        last = None
+        count = 1
+        lastNext = None
+        startPrev = None
+        flag = 0
         
-        while currentPosition < left:
-            start = currentNode
-            currentNode = currentNode.next
-            currentPosition += 1
-        tail = currentNode
-        reversedLinkedList = None
-        while currentPosition >= left and currentPosition <= right:
-            nxt = currentNode.next
-            currentNode.next = reversedLinkedList
-            reversedLinkedList = currentNode
-            currentNode = nxt
-            currentPosition += 1
+        while head:
+            if count == left:
+                start = head
+                flag = 1
+            elif count == right:
+                last = head
+                lastNext = head.next
+                break
+            count += 1
+            if flag == 0:
+                startPrev = head
+            head = head.next
         
-        start.next = reversedLinkedList
-        tail.next = currentNode
-        
-        if left >1:
-            return head
+        #print(start,last.val,startPrev.val,lastNext.val)
+        last.next = None
+        curr = start
+        prev =  None
+        next_ = None
+        while curr:
+            #print(curr)
+            next_ = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_
+        #print(tempHead,start,startPrev)
+        if startPrev != None:
+            startPrev.next = prev
         else:
-            return reversedLinkedList
+            tempHead = prev
+        start.next = lastNext
+        #print(start,tempHead,prev)
+        return tempHead
         
+            
         
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        
+        def solutionOne():
+            if left == right:
+                return head
+
+            start = head
+            prev = None
+            current = head
+            nextN = None
+
+            headTemp = head
+            currentPos = 1
+            while currentPos < left:
+                prev = head
+                head = head.next
+                currentPos+=1
+            if prev!=None:    
+                start = prev
+            #print(head,prev)
+            tail = head
+            prev = None
+            current = head
+            while(currentPos>=left and currentPos<=right):
+                #print(prev,current,"\n")
+                nextN = current.next
+                current.next = prev
+                prev = current
+                current = nextN
+                currentPos+=1
+
+            #prevLeft.next = prevLeft
+            #print(start,tail)
+            #tail = start.next
+            start.next = prev
+            tail.next = current
+            #headT.next = current
+            #prev.next = current
+            #print(prev,current,tail)
+            if left>1:
+                return headTemp
+            return prev
+        
+        return self.reverseBetweenTwo(head,left,right)
         
             
         
