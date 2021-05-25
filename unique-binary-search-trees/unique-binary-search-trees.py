f59f19ca82f0a46c664c4b067fd1eb1cc0cb50e8
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-26 00:04:28 +0530

    Time: 28 ms (77.80%), Space: 14.3 MB (45.48%) - LeetHub

--- unique-binary-search-trees/unique-binary-search-trees.py
+++ unique-binary-search-trees/unique-binary-search-trees.py
@@ -1,16 +1,35 @@
 class Solution:
-    def numTrees(self, n: int) -> int:
-        @cache
-        def recur(n):
-            if n<=1:
-                return 1
+
+    def catlanNumber(self,n,dp):
+        if n<=1:
+            return 1
+        if dp[n] == None:
             res = 0
             for i in range(n):
-                res+= recur(i)*recur(n-i-1)
-            
-            return res
+                res+=self.catlanNumber(i,dp)*self.catlanNumber(n-i-1,dp)
+            dp[n] = res
+            return dp[n]
+        else:
+            return dp[n]
+    
+    
+    def numTrees(self, n: int) -> int:
+        #@cache
+        dp = [None]*n
+        def recur(n,dp):
+            if n<=1:
+                return 1
+            if dp[n-1]!=None:
+                return dp[n-1]
+            res = 0
+            for i in range(n):
+                res+= recur(i,dp)*recur(n-i-1,dp)
+            dp[n-1] = res
+            return dp[n-1]
         #fact = factorial(n)
-        re = recur(n)
+        #re = recur(n,dp)
         #print(re,fact)
         #unique = recur(n)//factorial(n)
-        return re
+        #return re
+        dp = [None]*(n+1)
+        return self.catlanNumber(n,dp)
