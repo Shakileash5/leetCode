7536b281e1a495f9ee6a8f438c0d41375d846302
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-26 18:44:48 +0530

    Time: 24 ms (94.32%), Space: 14.3 MB (29.22%) - LeetHub

--- same-tree/same-tree.py
+++ same-tree/same-tree.py
@@ -5,9 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def isSameSolTwo(self,rootP,rootQ):
+        if rootP == None and rootQ == None:
+            print("hereBoth",rootP,rootQ)
+            return True
+        elif rootP == None and rootQ or rootQ == None and rootP:
+            print("here",rootP,rootQ)
+            return False
+        if rootP.val != rootQ.val:
+            return False
+        res1 = self.isSameSolTwo(rootP.left,rootQ.left)
+        res2 = self.isSameSolTwo(rootP.right,rootQ.right)
+        #print(rootP.val,rootQ.val)
+        if res1 == res2 and res1 == True:
+            return True
+        else:
+            return False
+    
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         
-        def isSame(root1,root2):
+        """ def isSame(root1,root2):
             if root1==None and root2==None:
                 return True
             elif root1!=None and root2!=None:
@@ -19,6 +36,7 @@ class Solution:
                         return True
                     else:
                         return False
-            return False
+            return False"""
         
-        return isSame(p,q)
+        #return isSame(p,q)
+        return self.isSameSolTwo(p,q)
