264cd51be66ea69f660ffdc6c25aa438d850d94f
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-26 13:16:10 +0530

    Time: 32 ms (57.60%), Space: 14 MB (98.81%) - LeetHub

--- swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -4,12 +4,70 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def swapPairsTwo(self,head):
+        length = 0
+        pointer = head
+        while pointer:
+            length += 1
+            pointer = pointer.next
+        if length < 2:
+            return head
+        
+        count = 1
+        prev = None
+        last = None
+        temp = None
+        while head:
+            #print(count,'initial head---.',head)
+            if count % 2 == 0:
+                #print(head)
+                if head.next is not None:
+                    #print('head.next not none------')
+                    if last is None:
+                        #print('last is none-----')
+                        temmp = head.next
+                        head.next = prev
+                        prev.next = temmp
+                        temp = head
+                    else:
+                        #print('last is not none------')
+                        temmp = head.next
+                        head.next = prev
+                        prev.next = temmp
+                        last.next = head
+                    head = temmp
+                    count += 1
+                    #print('after swapp head--->',temp)
+                    continue
+                else:
+                    #print('head.next is none')
+                    if last is None:
+                        #print('last is none')
+                        head.next = prev
+                        prev.next = None
+                        temp = head
+                    else:
+                        #print('last is not none----')
+                        head.next = prev
+                        prev.next = None
+                        last.next = head
+                    #print('after swapp head--->',temp)
+                    break
+                #print(temp)
+            last = prev
+            prev = head
+            head = head.next
+            count += 1
+        
+        #print('final--------->',temp)
+        return temp
+    
     def swapPairs(self, head: ListNode) -> ListNode:
         if head == None:
             return head
         curr = head
-        count = 0
         
+        """
         while curr!= None:
             if curr.next:
                 val = curr.next.val
@@ -18,7 +76,9 @@ class Solution:
                 curr = curr.next.next
             else:
                 break
-        """while curr.next!=None:
+        
+        count = 0
+        while curr.next!=None:
             if count%2==0: 
                 print("here")
                 temp = curr.next.val
@@ -27,6 +87,5 @@ class Solution:
             curr = curr.next
             count+=1"""
             
-        
-        return head
+        return self.swapPairsTwo(head)
            
