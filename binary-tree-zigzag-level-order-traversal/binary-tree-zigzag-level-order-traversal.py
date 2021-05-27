62c34a454068305822c9864ad8bab879734b1906
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-27 21:44:47 +0530

    Time: 32 ms (66.80%), Space: 14.7 MB (11.37%) - LeetHub

--- binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -5,38 +5,63 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+    def zigzagSolTwo(self,root):
         if root == None:
             return []
-        resQueqe = []
-        resQueqe.append(root)
-        resQueqeAlter = []
-        count = 0
-        zigZag = []
-        levelOrder =[]
+        res = []
+        resUtil = []
+        queue = []
+        queueUtil = []
+        count = 1
+        queue.append(root)
         
-        while(len(resQueqe)>0):
-            res = resQueqe.pop(0)
-            if len(levelOrder)<count+1:
-                print(count)
-                if (count)%2==0 and count !=0:
-                    levelOrder[-1] = levelOrder[-1][::-1]
-                    print(levelOrder,count)
-                levelOrder.append([res.val])
-            else:
-                levelOrder[count].append(res.val)
-            #print(res.val)
-            if res.left!=None:
-                resQueqeAlter.append(res.left)
-            if res.right!=None:
-                resQueqeAlter.append(res.right)
-            if len(resQueqe)==0:
-                #print(resQueqeAlter)
-                resQueqe = resQueqeAlter[:]
-                resQueqeAlter = []
+        while queue:
+            node = queue.pop(0)
+            resUtil.append(node.val)
+            if node.left:
+                queueUtil.append(node.left)
+            if node.right:
+                queueUtil.append(node.right)
+                
+            if queue==[]:
+                if count%2==0:
+                        resUtil = resUtil[::-1]
+                res.append(resUtil[:])
+                if queueUtil:
+                    queue = queueUtil.copy()
+                queueUtil = []
+                resUtil = []
                 count+=1
-        print(levelOrder,count)
-        if count%2==0:
-            levelOrder[-1] = levelOrder[-1][::-1]
-        return levelOrder
-            
+        return res
+        
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        def solutionOne():
+            if root == None:
+                return []
+            resQueqe = []
+            resQueqe.append(root)
+            resQueqeAlter = []
+            count = 0
+            zigZag = []
+
+            while(len(resQueqe)>0):
+                res = resQueqe.pop(0)
+                if len(zigZag)<count+1:
+                    if (count)%2==0 and count !=0:
+                        zigZag[-1] = zigZag[-1][::-1]
+                    zigZag.append([res.val])
+                else:
+                    zigZag[count].append(res.val)
+                if res.left!=None:
+                    resQueqeAlter.append(res.left)
+                if res.right!=None:
+                    resQueqeAlter.append(res.right)
+                if len(resQueqe)==0:
+                    resQueqe = resQueqeAlter[:]
+                    resQueqeAlter = []
+                    count+=1
+
+            if count%2==0:
+                zigZag[-1] = zigZag[-1][::-1]
+        #return zigZag
+        return self.zigzagSolTwo(root)    
