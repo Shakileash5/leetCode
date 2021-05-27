a554f0ec1ffa5baf25107d6bb3f06065dc8d8861
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-27 21:50:22 +0530

    Time: 36 ms (90.29%), Space: 15.9 MB (79.35%) - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -5,6 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def recurTwo(self,root):
+        if root == None:
+            return 0
+        height = max(self.recurTwo(root.left),self.recurTwo(root.right))+1
+        return height
+        
     def maxDepth(self, root: TreeNode) -> int:
         maxDepth = [-1]
         
@@ -18,6 +24,7 @@ class Solution:
             
             return
         
-        recur(root,0)
-        print(maxDepth[0])
-        return maxDepth[0]+1
+        #recur(root,0)
+        #print(maxDepth[0])
+        #return maxDepth[0]+1
+        return self.recurTwo(root)
