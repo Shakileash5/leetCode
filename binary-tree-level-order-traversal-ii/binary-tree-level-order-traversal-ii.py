30dfef692cc62f2967aca2728471d483f6059da4
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-27 21:57:53 +0530

    Time: 28 ms (94.00%), Space: 14.6 MB (43.60%) - LeetHub

--- binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
+++ binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
@@ -5,29 +5,56 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+    def levelOrderBottomTwo(self,root):
         if root == None:
             return []
+        res = []
+        resUtil = []
+        queue = []
+        queueUtil = []
+        queue.append(root)
+        while queue:
+            node = queue.pop(0)
+            resUtil.append(node.val)
+            if node.left:
+                queueUtil.append(node.left)
+            if node.right:
+                queueUtil.append(node.right)
+            if queue == []:
+                res.insert(0,resUtil[:])
+                if queueUtil:
+                    queue = queueUtil[:]
+                queueUtil = []
+                resUtil = []
+        return res
+            
         
-        resQueqe = []
-        resQueqe.append(root)
-        resQueqeAlter = []
-        count = 0
-        levelOrder = []
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        def solutionTwo():
+            if root == None:
+                return []
+
+            resQueqe = []
+            resQueqe.append(root)
+            resQueqeAlter = []
+            count = 0
+            levelOrder = []
+
+            while(len(resQueqe)>0):
+                res = resQueqe.pop(0)
+                if len(levelOrder)<count+1:
+                    levelOrder.append([res.val])
+                else:
+                    levelOrder[count].append(res.val)
+                if res.left!=None:
+                    resQueqeAlter.append(res.left)
+                if res.right!=None:
+                    resQueqeAlter.append(res.right)
+                if len(resQueqe)==0:
+                    resQueqe = resQueqeAlter[:]
+                    resQueqeAlter = []
+                    count+=1
+
+            return levelOrder[::-1]
         
-        while(len(resQueqe)>0):
-            res = resQueqe.pop(0)
-            if len(levelOrder)<count+1:
-                levelOrder.append([res.val])
-            else:
-                levelOrder[count].append(res.val)
-            if res.left!=None:
-                resQueqeAlter.append(res.left)
-            if res.right!=None:
-                resQueqeAlter.append(res.right)
-            if len(resQueqe)==0:
-                resQueqe = resQueqeAlter[:]
-                resQueqeAlter = []
-                count+=1
-                
-        return levelOrder[::-1]
+        return self.levelOrderBottomTwo(root)
