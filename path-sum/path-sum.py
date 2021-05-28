761e734ca19e55b102633131d6cac3b5958dd54a
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-28 17:36:38 +0530

    Time: 36 ms (93.74%), Space: 16.1 MB (23.38%) - LeetHub

--- path-sum/path-sum.py
+++ path-sum/path-sum.py
@@ -5,15 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def searchSum(self,root,sum_,target):
+        if root == None:
+            return 
+        
+        if root.left==None and root.right==None and sum_+root.val == target:
+            return True
+        
+        if self.searchSum(root.left,sum_+root.val,target):
+            return True
+        
+        if self.searchSum(root.right,sum_+root.val,target):
+            return True
+        return False
+    
     def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
         
         def recurInorder(root,pathSum):
             
             if root == None:
                 return None
-            
-            
-            
+    
             if recurInorder(root.left,pathSum+root.val) == True:
                 return True
             
@@ -23,10 +35,11 @@ class Solution:
             #print(root.val,pathSum+root.val)
             if root.left==None and root.right==None and pathSum+root.val == targetSum:
                 return True
+            
             return False
         
-        return recurInorder(root,0)
-            
+        #return recurInorder(root,0)
+        return self.searchSum(root,0,targetSum)    
             
             
         
