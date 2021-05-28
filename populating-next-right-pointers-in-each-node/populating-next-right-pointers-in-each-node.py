8541a5d43085fc036167f54d53c3a25e017911a9
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-28 20:33:50 +0530

    Time: 56 ms (91.61%), Space: 15.8 MB (30.60%) - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -9,35 +9,61 @@ class Node:
 """
 
 class Solution:
+    def connectLevel(self,root):
+        queue = []
+        queueUtil = []
+        queue.append(root)
+        
+        while queue:
+            node = queue.pop(0)
+            if queue == []:
+                node.next = None
+            else:
+                node.next = queue[0]
+            
+            if node.left:
+                queueUtil.append(node.left)
+            if node.right:
+                queueUtil.append(node.right)
+            
+            if queue == []:
+                if queueUtil:
+                    queue = queueUtil.copy()
+                queueUtil = []
+        return root
+        
+    
     def connect(self, root: 'Node') -> 'Node':
         
         if root==None:
             return None
         
-        Queqe = []
-        utilLevel = []
-        temp = root
-        Queqe.append(root)
-        last = None
-        while(len(Queqe)!=0):
-            root = Queqe.pop(0)
-            if root.left:
-                if last!=None:
-                    last.next = root.left
-                utilLevel.append(root.left)
-                last = root.left
-            if root.right:
-                last.next = root.right
-                utilLevel.append(root.right)
-                last = root.right
-                root.right.next = None
-            
-            if Queqe == []:
-                Queqe = utilLevel[:]
-                utilLevel = []
-                last = None
+        def solutionOne():
+            Queqe = []
+            utilLevel = []
+            temp = root
+            Queqe.append(root)
+            last = None
+            while(len(Queqe)!=0):
+                root = Queqe.pop(0)
+                if root.left:
+                    if last!=None:
+                        last.next = root.left
+                    utilLevel.append(root.left)
+                    last = root.left
+                if root.right:
+                    last.next = root.right
+                    utilLevel.append(root.right)
+                    last = root.right
+                    root.right.next = None
+
+                if Queqe == []:
+                    Queqe = utilLevel[:]
+                    utilLevel = []
+                    last = None
                 
         #print(levelOrder)
-        return temp
+        #return temp
+        return self.connectLevel(root)
         
             
