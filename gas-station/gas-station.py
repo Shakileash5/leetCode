0f6b1e1f35c5d60be0dd36e97a2fbdcd291f3d94
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-29 21:43:05 +0530

    Time: 4684 ms (6.99%), Space: 15.3 MB (29.31%) - LeetHub

--- gas-station/gas-station.py
+++ gas-station/gas-station.py
@@ -1,4 +1,29 @@
 class Solution:
+    def gasStation(self,gas,cost):
+        size = len(gas)
+        
+        def canReach(idx):
+            if gas[idx]<cost[idx]:
+                return False
+            start = idx
+            gasAvailable = gas[idx]
+            gasAvailable -= cost[idx]
+            idx = (idx+1)%size
+            while start!=idx:
+                gasAvailable += gas[idx]
+                if gasAvailable<cost[idx]:
+                    return False
+                gasAvailable -= cost[idx]
+                idx = (idx+1)%size
+            return True
+        
+        #res = canReach(3)
+        for i in range(size):
+            if canReach(i):
+                return i
+        return -1
+            
+            
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         
         def travel(start,gas,cost,size)->bool:
@@ -24,9 +49,11 @@ class Solution:
             return True
         size = len(gas)
         #res = travel(4,gas,cost,size)
-        for i in range(size):
-            res = travel(i,gas,cost,size)
-            if res == True:
-                return i
+        #for i in range(size):
+        #    res = travel(i,gas,cost,size)
+        #    if res == True:
+        #        return i
         
-        return -1
+        #return -1
+        
+        return self.gasStation(gas,cost)
