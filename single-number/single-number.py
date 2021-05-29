5388439fbb172c548de668009ffea838e85cc9a1
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-29 20:29:41 +0530

    Time: 164 ms (20.77%), Space: 16.6 MB (58.56%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,20 +1,24 @@
 class Solution:
+    def singleNumberTwo(self,nums):
+        xor = nums[0]
+        size = len(nums)
+        for i in range(1,size):
+            xor ^= nums[i]
+        return(xor)
+    
     def singleNumber(self, nums: List[int]) -> int:
         
         singleNumbers = set()
         hashNums = {}
-        #for i in range(10):
-        #    hashNums[i] = 0
+        def naive():
+            for i in nums:
+                keys = hashNums.keys()
+                if i not in keys:
+                    singleNumbers.add(i)
+                    hashNums[i] = 1
+                else:
+                    singleNumbers.remove(i)
+            singleNumbers = list(singleNumbers)
         
-        size = len(nums)
-        
-        for i in nums:
-            keys = hashNums.keys()
-            if i not in keys:
-                singleNumbers.add(i)
-                hashNums[i] = 1
-            else:
-                singleNumbers.remove(i)
-        singleNumbers = list(singleNumbers)
-        
-        return singleNumbers[0]
+        #return singleNumbers[0]
+        return self.singleNumberTwo(nums)
