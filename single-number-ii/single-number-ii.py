263adc2a77fe8929fd70d71811d7fbd03ca50432
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-29 21:19:47 +0530

    Time: 68 ms (28.21%), Space: 16.2 MB (10.41%) - LeetHub

--- single-number-ii/single-number-ii.py
+++ single-number-ii/single-number-ii.py
@@ -1,17 +1,32 @@
 class Solution:
+    def findDuplicates(self,nums):
+        hashMap = {}
+        for val in nums:
+            if val not in hashMap:
+                hashMap[val] = True
+            else:
+                hashMap[val] = False
+        for key in hashMap.keys():
+            if hashMap[key] == True:
+                return key
+        return -1
+        
     def singleNumber(self, nums: List[int]) -> int:
         
-        singleNumbers = set()
-        hashNums = {}
+        def naiveSol():
+            singleNumbers = set()
+            hashNums = {}
+
+            for i in nums:
+                keys = hashNums.keys()
+                if i not in keys:
+                    singleNumbers.add(i)
+                    hashNums[i] = 1
+                elif i in keys:
+                    if i in singleNumbers:
+                        singleNumbers.remove(i)
+            singleNumbers = list(singleNumbers)
+
+            return singleNumbers[0]
         
-        for i in nums:
-            keys = hashNums.keys()
-            if i not in keys:
-                singleNumbers.add(i)
-                hashNums[i] = 1
-            elif i in keys:
-                if i in singleNumbers:
-                    singleNumbers.remove(i)
-        singleNumbers = list(singleNumbers)
-        
-        return singleNumbers[0]
+        return self.findDuplicates(nums)
