375cd58eda1d7f177eea592194a3ca81cedf37f2
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-30 21:56:47 +0530

    Time: 44 ms (30.60%), Space: 14.7 MB (12.68%) - LeetHub

--- word-break/word-break.py
+++ word-break/word-break.py
@@ -1,4 +1,40 @@
 class Solution:
+    def wordBreakTwo(self,s,words):
+        size = len(s)
+        flag = [False]
+        def recur(idx,temp):
+            if idx>=size:
+                #print(temp)
+                flag[0]=True
+                return 
+            for i in range(idx,size):
+                subStr = s[idx:i+1]
+                if subStr in words:
+                    recur(i+1,temp+[subStr])
+            return
+        
+        #recur(0,[])   
+        def bottomUp(idx):
+            if idx>=size:
+                return True
+            if dp[idx] == None:
+            
+                for i in range(idx,size):
+                    subStr = s[idx:i+1]
+                    if subStr in words:
+                        res = bottomUp(i+1)
+                        if res:
+                            dp[idx] = True
+                            return True
+                dp[idx] = False
+                return dp[idx]
+                        
+            else:
+                return dp[idx]
+        
+        dp = [None]*(size+1)
+        return bottomUp(0)
+    
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
         
@@ -40,14 +76,14 @@ class Solution:
         #size = len(s)
         #words = []
         #recur(0,'')
-        wordDict = set(wordDict)
+        """wordDict = set(wordDict)
         dp = [True] + [False] * len(s)
         for i in range(len(s)):
             if dp[i]:
                 for j in range(i, len(dp)):
                     if s[i:j] in wordDict:
                         dp[j] = True
-        return dp[-1]
-
+        #return dp[-1]"""
+        return self.wordBreakTwo(s,wordDict)
         
         
