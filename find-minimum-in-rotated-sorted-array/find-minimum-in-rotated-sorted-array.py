3e4a2d77cf4c83b664d22283a00cb44b74a405ea
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-31 21:38:38 +0530

    Time: 88 ms (6.05%), Space: 14.7 MB (26.69%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,4 +1,25 @@
 class Solution:
+    def findPivot(self,left,right,nums):
+        mid = (left+right)//2
+        #print(left,right,mid,left>right)
+        if nums[left] < nums[right]:
+            return left
+        if left>right:
+            #print("fe")
+            return -1
+        
+        if mid>0 and mid<(len(nums)-1) and nums[mid-1]<=nums[mid]>=nums[mid+1]:
+            return mid+1
+        if mid>0 and mid<(len(nums)-1) and nums[mid-1]>=nums[mid]<=nums[mid+1]:
+            return mid
+        #print(left,right)
+        if nums[left]<=nums[mid] and nums[mid]>=nums[right]:
+            return self.findPivot(mid+1,right,nums)
+        if nums[left]>=nums[mid] and nums[mid]<=nums[right]:
+            return self.findPivot(left,mid-1,nums)
+        
+                
+    
     def findMin(self, nums: List[int]) -> int:
         def binaryPivot(nums,left,right):
             mid = (left+right)//2
@@ -23,10 +44,15 @@ class Solution:
         
         size = len(nums)
         
-        pivot = binaryPivot(nums,0,size-1)
+        """pivot = binaryPivot(nums,0,size-1)
         #print("pivot",pivot)
+        
         if pivot+1<size:
             if nums[pivot+1]<nums[pivot]:
                 return nums[pivot+1]
-        return nums[0]
-        
+        return nums[0]"""
+        if size == 1:
+            return nums[0]
+        elif size == 2:
+            return min(nums)
+        return nums[self.findPivot(0,len(nums)-1,nums)]
