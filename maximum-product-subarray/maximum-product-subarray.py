298343c4c8949e7f080362939db4b62d1d048a96
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-31 20:39:08 +0530

    Time: 112 ms (5.06%), Space: 14.3 MB (61.52%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.py
+++ maximum-product-subarray/maximum-product-subarray.py
@@ -1,29 +1,51 @@
 class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        
+    def kadanesMagic(self,nums):
+        maxEndingHere = 1
+        maxSoFar = -2**10
         size = len(nums)
-        if size == 1:
-            return nums[0]
-        
-        maxSoFar = -2**31
-        maxEnding = 1
-        
         for i in range(size):
-            maxEnding = maxEnding*nums[i]
-            if maxEnding>maxSoFar:
-                maxSoFar = maxEnding
-            if maxEnding == 0:
-                maxEnding = 1
-                
-        maxTemp = maxSoFar
-        maxEnding = 1
+            maxEndingHere *= nums[i]
+            maxSoFar = max(maxSoFar,maxEndingHere)
+            if maxEndingHere==0:
+                maxEndingHere = 1
+        
+        maxEndingHere = 1
         for i in range(size-1,-1,-1):
-            maxEnding = maxEnding*nums[i]
-            
-            if maxEnding>maxSoFar:
-                maxSoFar = maxEnding
-            if maxEnding == 0:
-                maxEnding = 1
+            maxEndingHere *= nums[i]
+            maxSoFar = max(maxSoFar,maxEndingHere)
+            if maxEndingHere==0:
+                maxEndingHere = 1
         
         return maxSoFar
+    
+    def maxProduct(self, nums: List[int]) -> int:
+        
+        def kadaneMagicUnknown():
+            size = len(nums)
+            if size == 1:
+                return nums[0]
+
+            maxSoFar = -2**31
+            maxEnding = 1
+
+            for i in range(size):
+                maxEnding = maxEnding*nums[i]
+                if maxEnding>maxSoFar:
+                    maxSoFar = maxEnding
+                if maxEnding == 0:
+                    maxEnding = 1
+
+            maxTemp = maxSoFar
+            maxEnding = 1
+            for i in range(size-1,-1,-1):
+                maxEnding = maxEnding*nums[i]
+
+                if maxEnding>maxSoFar:
+                    maxSoFar = maxEnding
+                if maxEnding == 0:
+                    maxEnding = 1
+
+            return maxSoFar
+        
+        return self.kadanesMagic(nums)
                 
