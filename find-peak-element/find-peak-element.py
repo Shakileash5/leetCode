fd506889e87007ed5864c673136451fb35a3f4d7
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-05-31 23:54:05 +0530

    Time: 48 ms (42.62%), Space: 14.6 MB (9.01%) - LeetHub

--- find-peak-element/find-peak-element.py
+++ find-peak-element/find-peak-element.py
@@ -1,4 +1,19 @@
 class Solution:
+    def binarySearch(self,left,right,nums):
+        mid = (left+right)//2
+        
+        if mid == 0 and nums[mid+1]<nums[mid]:
+            return mid
+        elif mid == len(nums)-1 and nums[mid-1]<nums[mid]:
+            return mid
+        
+        if nums[mid-1]<nums[mid]>nums[mid+1]:
+            return mid
+        if nums[mid+1]>nums[mid]:
+            return self.binarySearch(mid+1,right,nums)
+        if nums[mid-1]>nums[mid]:
+            return self.binarySearch(left,mid-1,nums)
+    
     def findPeakElement(self, nums: List[int]) -> int:
         
         size = len(nums)
@@ -9,16 +24,17 @@ class Solution:
                 return 0
             else:
                 return 1
+        def naive():
+            for i in range(0,size):
+                if i == size-1:
+                    if nums[i]>nums[i-1]:
+                        return i
+                if i == 0:
+                    if nums[i]>nums[i+1]:
+                        return i
+
+                if nums[i]>nums[i+1] and nums[i]>nums[i-1]:
+                    return i
+            return -1
         
-        for i in range(0,size):
-            print(i-1,i,i+1)
-            if i == size-1:
-                if nums[i]>nums[i-1]:
-                    return i
-            if i == 0:
-                if nums[i]>nums[i+1]:
-                    return i
-                
-            if nums[i]>nums[i+1] and nums[i]>nums[i-1]:
-                return i
-        return -1
+        return self.binarySearch(0,size-1,nums)
