d7e066a8be598507df486446d5ae3fbb19090ee4
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-01 22:26:45 +0530

    Time: 128 ms (5.56%), Space: 27.8 MB (22.47%) - LeetHub

--- repeated-dna-sequences/repeated-dna-sequences.py
+++ repeated-dna-sequences/repeated-dna-sequences.py
@@ -1,16 +1,42 @@
 class Solution:
-    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+    
+    def slidingWindowWithHash(self,s):
         size = len(s)
         hashMap = {}
-        repeatingSequence = []
-        
+        res = []
+        #print(window)
+        #print(size)
+        if size<=10:
+            return res
         for i in range(size):
-            subString = s[i:i+10]
-            #print(subString)
-            if subString in hashMap:
-                if subString not in repeatingSequence:
-                    repeatingSequence.append(subString)
-            else:
-                hashMap[subString] = 1
+            window = s[i:i+10]
+            #print(hashMap,10+i,i,s[10+i])
+            if len(window) == 10:
+                if window not in hashMap:
+                    hashMap[window] = 1
+                else:
+                    if hashMap[window] == 1:
+                        #print(window)
+                        res.append(window)
+                    hashMap[window] += 1
+        return res
+    
+    
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
         
-        return (repeatingSequence)
+        def solutionOne():
+            size = len(s)
+            hashMap = {}
+            repeatingSequence = []        
+            for i in range(size):
+                subString = s[i:i+10]
+                #print(subString)
+                if subString in hashMap:
+                    if subString not in repeatingSequence:
+                        repeatingSequence.append(subString)
+                else:
+                    hashMap[subString] = 1
+
+            return (repeatingSequence)
+        
+        return self.slidingWindowWithHash(s)
