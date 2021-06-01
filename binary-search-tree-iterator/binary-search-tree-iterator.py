6af079a1838b1d69fc59448dba5600feeb306baf
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-01 13:05:52 +0530

    Time: 124 ms (5.14%), Space: 20.3 MB (80.20%) - LeetHub

--- binary-search-tree-iterator/binary-search-tree-iterator.py
+++ binary-search-tree-iterator/binary-search-tree-iterator.py
@@ -5,55 +5,26 @@
 #         self.left = left
 #         self.right = right
 class BSTIterator:
-            
-    def __init__(self, root: TreeNode):
-        def nextGenerator(node):
-            if node.left:
-                yield from nextGenerator(node.left)
-            yield node.val
-            if node.right:
-                yield from nextGenerator(node.right)
-                
-        self.generator = nextGenerator(root)
-        self.root = next(self.generator,None)
-            
-    def next(self)->int:
-        temp = self.root
-        self.root = next(self.generator,None)
-        return temp
+    def inorder(self,root):
+        if root == None:
+            return 
+        self.inorder(root.left)
+        self.inorderStack.append(root.val)
+        self.inorder(root.right)
         
-    def next_try(self) -> int:
-            print("before",self.stack,self.checked)
-            while(self.stack):
-                if self.checked == False:
-                    if self.root.right:
-                            self.nextStack.append(1)
-                    if self.root.left:
-                        self.stack.append(self.root.left)
-                        print("added",self.root.left.val)
-                        self.root = self.root.left
-                        continue
-                
-                
-                if self.checked == False:
-                    node = self.stack.pop()
-                    self.root = node
-                    self.checked = True
-                    print(self.root.val)
-                    return self.root.val
-                self.checked = False
-                if self.root.right:
-                    self.nextStack.pop()
-                    self.stack.append(self.root.right)
-                    self.root = self.root.right
-            print("After",self.stack)
-            return self.root
-
+    def __init__(self, root: TreeNode):
+        self.inorderStack = []
+        self.inorder(root)
+        self.size = len(self.inorderStack)
+        self.ptr = 0
+        
+    def next(self) -> int:
+        val = self.inorderStack[self.ptr]
+        self.ptr += 1
+        return val
+        
     def hasNext(self) -> bool:
-        #print(self.root.val,"called",self.root.right!=None)
-        #node = self.stack[-1]
-        #print(self.stack,self.nextStack)
-        if self.root!=None:
+        if self.ptr<=(self.size-1):
             return True
         return False
 
