000b881ebf657f031b16f46752b033370bf4cc8d
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-02 13:25:13 +0530

    Time: 36 ms (54.35%), Space: 14.2 MB (46.78%) - LeetHub

--- happy-number/happy-number.py
+++ happy-number/happy-number.py
@@ -1,4 +1,24 @@
 class Solution:
+    def happyNumber(self,n):
+        def sumOfSqDigits(num):
+            res = 0
+            while num:
+                rem = num%10
+                num//=10
+                res+=rem*rem
+            return res
+        
+        hardCp = n
+        hashMap = {}
+        while n!=1:
+            hashMap[n] = 1
+            n = sumOfSqDigits(n)
+            if n in hashMap:
+                return False
+        if n == 1:
+            return True
+        return False
+        
     def isHappy(self, n: int) -> bool:
         def sumOfSqDigits(num):
             sum = 0
@@ -6,13 +26,17 @@ class Solution:
                 sum += (num%10)**2
                 num //= 10
             return sum
-        num = n
-        hashMap = {}
-        while(num!=1):
-            hashMap[num] = 1
-            num = sumOfSqDigits(num)
-            if num in hashMap:
-                return False
-        return True
+        
+        def solutionOne():
+            num = n
+            hashMap = {}
+            while(num!=1):
+                hashMap[num] = 1
+                num = sumOfSqDigits(num)
+                if num in hashMap:
+                    return False
+            return True
+        
+        return self.happyNumber(n)
         
         
