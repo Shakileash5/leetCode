358ea794b6fe57bc1eafee63a4335fdf4233f2ea
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-02 22:47:25 +0530

    Time: 100 ms (40.28%), Space: 21.8 MB (53.66%) - LeetHub

--- contains-duplicate-ii/contains-duplicate-ii.py
+++ contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,18 +1,35 @@
 class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        
-        hashMap = {}
+    def SolutionTwo(self,nums,k):
         size = len(nums)
-        
+        hashMap = {}
         for i in range(size):
             if nums[i] not in hashMap:
                 hashMap[nums[i]] = i
             else:
-                idxI = hashMap[nums[i]]
-                val = abs(idxI-i)
+                j = hashMap[nums[i]]
+                val = abs(i-j)
                 if val<=k:
                     return True
-                else:
-                    hashMap[nums[i]] = i
-        
+                hashMap[nums[i]] = i
         return False
+    
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        
+        def solutionOne():
+            hashMap = {}
+            size = len(nums)
+
+            for i in range(size):
+                if nums[i] not in hashMap:
+                    hashMap[nums[i]] = i
+                else:
+                    idxI = hashMap[nums[i]]
+                    val = abs(idxI-i)
+                    if val<=k:
+                        return True
+                    else:
+                        hashMap[nums[i]] = i
+
+            return False
+        
+        return self.SolutionTwo(nums,k) 
