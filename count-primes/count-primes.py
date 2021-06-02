1255881cb63427e7c02213f65b985ad6a8255d7a
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-02 20:14:48 +0530

    Time: 1716 ms (52.81%), Space: 74.1 MB (40.92%) - LeetHub

--- count-primes/count-primes.py
+++ count-primes/count-primes.py
@@ -1,4 +1,25 @@
 class Solution:
+    def countPrimesTwo(self,n):
+        if n == 2:
+            return 0
+        
+        table = [True for _ in range(n+1)]
+        
+        def seiveMethod(table):
+            i = 2
+            while(i*i<=n):
+                if table[i]:
+                    for j in range(i*i,n,i):
+                        table[j] = False
+                i+=1
+            return
+        seiveMethod(table)
+        count = 0
+        for i in range(2,n):
+            if table[i]:
+                count+=1
+        return count
+    
     def countPrimes(self, n: int) -> int:
         if n==2:
             return 0
@@ -13,11 +34,13 @@ class Solution:
                 i+=1
             return(seiveTable)
         
-        table = seiveMethod(n)
-        res = []
-        print("fe",table,n)
-        for i in range(2,n):
-            if table[i]:
-                #print(i,end=' ')
-                res.append(i)
-        return len(res)
+        def solutionOne():
+            table = seiveMethod(n)
+            res = []
+            print("fe",table,n)
+            for i in range(2,n):
+                if table[i]:
+                    #print(i,end=' ')
+                    res.append(i)
+            return len(res)
+        return self.countPrimesTwo(n)
