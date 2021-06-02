59fc526cf57300745a8c15ee64b8aebb177052f1
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-02 22:35:10 +0530

    Time: 204 ms (8.79%), Space: 16.7 MB (37.82%) - LeetHub

--- course-schedule/course-schedule.py
+++ course-schedule/course-schedule.py
@@ -1,53 +1,86 @@
 class Graph:
-    def __init__(self,vertices):
-        self.graphNode = {}
+    def __init__(self,vertices=0):
+        self.nodes = {}
         self.vertices = vertices
-        
+    
     def addEdge(self,source,destination):
-        if source not in self.graphNode:
-            self.graphNode[source] = [destination]
+        if source in self.nodes:
+            self.nodes[source].append(destination)
         else:
-            self.graphNode[source].append(destination)
+            self.nodes[source] = [destination]
+    
+    def dfsUtil(self,node,visited):
+        if node in visited:
+            return
+        print(node)
+        visited.add(node)
+        if node in self.nodes:
+            for vertices in self.nodes[node]:
+                if vertices not in visited:
+                    self.dfsUtil(vertices,visited)
         return
     
-class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        courses = [_ for _ in range(numCourses)]
+    def dfs(self):
+        visited = set()
+        for node in self.nodes:
+            if node not in visited:
+                self.dfsUtil(node,visited)
+        return
+    
+    def topologySortUtil(self,node,visited,stack):
+        if node in visited:
+            return
+        #print(node)
+        visited.add(node)
+        if node in self.nodes:
+            for vertices in self.nodes[node]:
+                if vertices not in visited:
+                    self.topologySortUtil(vertices,visited,stack)
+        stack.append(node)
+        return    
         
-        def dfs(node):
-            
-            if node not in graph.graphNode:
-                return True
-            
-            if node in visitedSet:
-                return True
-            
-            if node in recursionSet:
-                return False
-            
-            recursionSet.add(node)
-            #print("added",node)
-            #if node in graph.graphNode.keys():
-            for nodes in graph.graphNode[node]:
-                if dfs(nodes) == False:
-                    return False
-            #else:
-            #    return True
-            visitedSet.add(node)
-            #print("no",node)
+    def topologySort(self):
+        visited = set()
+        stack = []
+        for node in self.nodes:
+            if node not in visited:
+                self.topologySortUtil(node,visited,stack)
+        
+        return stack
+    
+    def cycleUtil(self,node,visited,stack):
+        if node not in self.nodes:
             return True
+        if node in visited:
+            return True
+        if node in stack:
+            return False
         
-        graph = Graph(numCourses)
-        for i in prerequisites:
-            graph.addEdge(i[0],i[1])
-        
-        print(graph.graphNode)
-        visitedSet = set()
-        
-        for i in courses:
-            recursionSet = set()
-            if dfs(i) == False:
+        stack.add(node)
+        for vertice in self.nodes[node]:
+            if self.cycleUtil(vertice,visited,stack) == False:
                 return False
-                
+        visited.add(node)
+        return True
+    
+    def isCycle(self):
+        visited = set()
+        for node in self.nodes:
+            stack = set()
+            if self.cycleUtil(node,visited,stack) == False:
+                return False
         return True
             
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        graphObj = Graph(numCourses)
+        
+        for vertices_ in prerequisites:
+            graphObj.addEdge(vertices_[0],vertices_[1])
+        
+        #print(graphObj.nodes)
+        #visited = set()
+        #graphObj.dfs()
+        #print(graphObj.topologySort())
+        return graphObj.isCycle()
