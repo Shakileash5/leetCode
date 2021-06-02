e405c8f50dfa9765edbea24ab4da1bd1573dbdfe
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-02 20:40:50 +0530

    Time: 48 ms (28.51%), Space: 14.5 MB (29.84%) - LeetHub

--- isomorphic-strings/isomorphic-strings.py
+++ isomorphic-strings/isomorphic-strings.py
@@ -1,36 +1,53 @@
 class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
+    def isIsomorphicTwo(self,s,t):
         hashMapS = {}
         hashMapT = {}
+        size = len(s)
         
-        '''for i in range(len(s)):
-            print(s[i],t[i],s[i] != t[i])
-            if s[i] != t[i]:
-                print(s[i],t[i],"notmat",hashMap)
-                if t[i] not in hashMap:
-                    print(t[i],"not in")
-                    hashMap[t[i]] = 1
-                    s = s.replace(s[i],t[i])
-                else:
-                    return False
-            hashMap[s[i]] = 1
-            print(s,t,"\n")
-        if s != t:
-            return False'''
-        for i in range(len(s)):
-            flag = 0
-            if t[i] not in hashMapT:
-                hashMapT[t[i]] = [j for j, x in enumerate(t) if x == t[i]]
-                #print(hashMapT,t[i])
-                if s[i] not in hashMapS:
-                    hashMapS[s[i]] = [k for k, x in enumerate(s) if x == s[i]]
-                    #print(hashMapS,s[i])
-                    if hashMapT[t[i]] != hashMapS[s[i]]:
-                        return False
-                else:
-                    return False
+        for i in range(size):
+            flag = -1
+            if s[i] in hashMapS:
+                flag = hashMapS[s[i]]
+                hashMapS[s[i]] = i
             else:
-                if s[i] not in hashMapS:
+                hashMapS[s[i]] = i
+            if flag == -1:
+                if t[i] in hashMapT:
                     return False
+                hashMapT[t[i]] = i
+            else:
+                if t[i] not in hashMapT:
+                    return False
+                if hashMapT[t[i]] != flag:
+                    return False
+                hashMapT[t[i]] = i
         
         return True
+                
+        
+    
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        
+        def solutionOne():
+            hashMapS = {}
+            hashMapT = {}
+        
+            for i in range(len(s)):
+                flag = 0
+                if t[i] not in hashMapT:
+                    hashMapT[t[i]] = [j for j, x in enumerate(t) if x == t[i]]
+                    #print(hashMapT,t[i])
+                    if s[i] not in hashMapS:
+                        hashMapS[s[i]] = [k for k, x in enumerate(s) if x == s[i]]
+                        #print(hashMapS,s[i])
+                        if hashMapT[t[i]] != hashMapS[s[i]]:
+                            return False
+                    else:
+                        return False
+                else:
+                    if s[i] not in hashMapS:
+                        return False
+
+            return True
+        
+        return self.isIsomorphicTwo(s,t)
