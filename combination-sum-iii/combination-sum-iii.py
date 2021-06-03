fa7b8e43caee4a735c2b9b59ecb5a5fa242751de
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-03 21:47:27 +0530

    Time: 52 ms (6.43%), Space: 14.3 MB (58.94%) - LeetHub

--- combination-sum-iii/combination-sum-iii.py
+++ combination-sum-iii/combination-sum-iii.py
@@ -1,16 +1,24 @@
 class Solution:
+    def backtrackTwo(self,idx,temp,n,sum_,res,k):
+        if len(temp)==n:
+            #print(temp,sum_)
+            if sum_ == k:
+                res.append(temp)
+            return
+        for i in range(idx,10):
+            self.backtrackTwo(i+1,temp+[i],n,sum_+i,res,k)
+        return
+    
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         
         size = 9
         res = []
         
         def backtrack(idx,temp):
-            
             if len(temp)>=k:
                 sumTemp = sum(temp)        
                 if sumTemp==n:
                     res.append(temp)
-        
                 return
             
             for i in range(idx,size+1):
@@ -18,7 +26,7 @@ class Solution:
             
             return
         
-        backtrack(1,[])
+        #backtrack(1,[])
         #print(res)
-        
+        self.backtrackTwo(1,[],k,0,res,n)
         return res
