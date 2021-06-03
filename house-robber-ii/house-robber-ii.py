5d131fab92f6630523416a8b273cfbac38c5f6b9
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-03 15:50:09 +0530

    Time: 28 ms (84.14%), Space: 14.6 MB (5.49%) - LeetHub

--- house-robber-ii/house-robber-ii.py
+++ house-robber-ii/house-robber-ii.py
@@ -1,4 +1,17 @@
 class Solution:
+    
+    def robHouses(self,idx,nums,dp):
+        #print("d",idx,nums)
+        if idx>=len(nums):
+            return 0
+        if dp[idx] == None:
+            dontRob = self.robHouses(idx+1,nums,dp)
+            robNow = self.robHouses(idx+2,nums,dp) + nums[idx]
+            dp[idx] = max(dontRob,robNow)
+            return dp[idx]
+        else:
+            return dp[idx]
+    
     def solver(self,nums):
             
             @lru_cache(None)
@@ -16,32 +29,12 @@ class Solution:
         size = len(nums)
         if size == 1:
             return nums[0]
-        def recur(temp,idx):
-            if idx>=size-1:
-                sum = 0
-                for i in temp:
-                    if len(i)!=0:
-                        sum+=i[0]
-                print("flag::: ",temp,idx,recur.mark,sum,idx!=size-1 )
-                if recur.maxi<sum:
-                    if idx == size-1 and recur.mark == 0:
-                        print("true")
-                    else:
-                    #recur.mark = idx
-                        recur.maxi = max(recur.maxi,sum)
-                        print(recur.maxi)
-                
-                return
-            
-            
-            recur(temp+[nums[idx+2:idx+3]],idx+2)
-            recur(temp+[nums[idx+3:idx+4]],idx+3)
-            return
         
-        #recur.maxi = -1
-        #recur.mark = 0
-        #recur([[nums[0]]],0)
-        #recur.mark = 1
-        #recur([[nums[1]]],1)
+        #
+        #dp = [None]*(size+1)
+        #rob1StHouse = self.robHouses(2,nums[:-1],dp)
+        #dontRob1stHouse = self.robHouses(1,nums,dp)
         
+        #print(rob1StHouse,dontRob1stHouse)
+        #return max(rob1StHouse,dontRob1stHouse)
         return max(self.solver(nums[:-1]), self.solver(nums[1:])) 
