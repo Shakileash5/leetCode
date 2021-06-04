a4d215589d830e9e12c9ba9bf05900d0afb86419
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-04 18:01:04 +0530

    Time: 300 ms (5.40%), Space: 20.6 MB (64.80%) - LeetHub

--- search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -1,17 +1,37 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    def searchMatric2D(self,matrix,target):
         sizeR = len(matrix)
         sizeC = len(matrix[0])
-        
         x = 0
-        y = sizeC-1
+        y = sizeC - 1
         
         while(x<sizeR and y>=0):
-            if matrix[x][y] == target:
-                return True
-            if target>matrix[x][y]:
-                x+=1
+            if matrix[x][y]>target:
+                y -= 1
+            elif matrix[x][y]<target:
+                x += 1
             else:
-                y-=1
-        
+                return True
         return False
+        
+        
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        def searchMatrixOne():
+            sizeR = len(matrix)
+            sizeC = len(matrix[0])
+
+            x = 0
+            y = sizeC-1
+
+            while(x<sizeR and y>=0):
+                if matrix[x][y] == target:
+                    return True
+                if target>matrix[x][y]:
+                    x+=1
+                else:
+                    y-=1
+
+            return False
+        
+        return self.searchMatric2D(matrix,target)
