b4c60a6a9fd03e325dff24dabd96bf13ecc495a8
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-04 22:56:47 +0530

    Time: 416 ms (5.09%), Space: 21.3 MB (40.25%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.py
+++ product-of-array-except-self/product-of-array-except-self.py
@@ -1,30 +1,61 @@
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+    def productExceptSelfTwo(self,nums):
+        product = 1
+        zeroFlag = 0
+        multipleZeros = 0
+        size = len(nums)
+        res = []
         
-        totalProduct = 1
-        flag = 0
-        for i in nums:
-            if  i == 0:
-                flag += 1
+        for i in range(size):
+            if nums[i] == 0:
+                if zeroFlag == 1:
+                    multipleZeros = 1
+                zeroFlag = 1
                 continue
-            totalProduct *=i
-
-        print(totalProduct,flag)
-        for i in range(len(nums)):
-            if flag>1:
-                nums[i] = 0
+            product *= nums[i]
+        
+        for i in range(size):
+            if multipleZeros:
+                res.append(0)
             else:
                 if nums[i] == 0:
-                    if flag == len(nums):
-                        nums[i] = 0
-                    else:
-
-                        nums[i] = totalProduct
+                    res.append(product)
+                elif zeroFlag == 1:
+                    res.append(0)
                 else:
-                    if flag > 0:
-                        nums[i] = 0
-                    else:
-
-                        nums[i] = totalProduct//nums[i]
+                    res.append(product//nums[i])
         
-        return nums
+        return res
+    
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        def solutionOne():
+            totalProduct = 1
+            flag = 0
+            for i in nums:
+                if  i == 0:
+                    flag += 1
+                    continue
+                totalProduct *=i
+
+            #print(totalProduct,flag)
+            for i in range(len(nums)):
+                if flag>1:
+                    nums[i] = 0
+                else:
+                    if nums[i] == 0:
+                        if flag == len(nums):
+                            nums[i] = 0
+                        else:
+
+                            nums[i] = totalProduct
+                    else:
+                        if flag > 0:
+                            nums[i] = 0
+                        else:
+
+                            nums[i] = totalProduct//nums[i]
+
+            return nums
+        
+        return self.productExceptSelfTwo(nums)
