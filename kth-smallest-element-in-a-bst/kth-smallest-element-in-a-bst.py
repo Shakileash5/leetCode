bffb5b54c2b30133368c2255fcc96617c15e3188
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-04 17:51:46 +0530

    Time: 64 ms (9.08%), Space: 18.2 MB (13.54%) - LeetHub

--- kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -5,6 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def recur(self,root,k):
+        if root == None:
+            return False
+        left = self.recur(root.left,k)
+        if left  == True:
+            return True
+        if self.count == k:
+            self.val = root.val
+            return True
+        self.count+=1
+        right = self.recur(root.right,k)
+        if right == True:
+            return True
+        return False
+        
+    
     def kthSmallest(self, root: TreeNode, k: int) -> int:
         
         def inorder(root):
@@ -14,15 +30,18 @@ class Solution:
                 return 1
             if inorder.count == k:
                 inorder.val = root.val
-                
                 return 1
             inorder.count+=1
             if inorder(root.right) == 1:
                 return 1
         
-        inorder.count = 1
-        inorder.val = -1
-        inorder(root)
-        
-        return inorder.val
+        #inorder.count = 1
+        #inorder.val = -1
+        #inorder(root)
+        #
+        #return inorder.val
+        self.count = 1
+        self.val = 0
+        self.recur(root,k)
+        return self.val
         
