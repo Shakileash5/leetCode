b91c12cdd2d31c5a54eb7c1aac8ac1bca1f2d8a8
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-06 21:07:21 +0530

    Time: 200 ms (18.18%), Space: 15.5 MB (49.48%) - LeetHub

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -1,7 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        i = 0
         size = len(nums)
-        actualSum = (size*(size+1))//2
-        arrSum = sum(nums)
-        num = actualSum - arrSum
-        return num
+        sum_ = (size*(size+1))//2
+        sumOrginal = 0
+        for val in nums:
+            sumOrginal += val
+        
+        return sum_ - sumOrginal
