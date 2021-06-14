aba2b984c15c0d42e1218570b3c75785c94d6a51
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-06-14 22:59:13 +0530

    Time: 36 ms (85.59%), Space: 14.8 MB (7.36%) - LeetHub

--- combination-sum-iv/combination-sum-iv.py
+++ combination-sum-iv/combination-sum-iv.py
@@ -1,9 +1,30 @@
 class Solution:
+    def combinationWithDP(self,nums,target):
+        size = len(nums)
+        dp = [None for i in range(target+1)]
+        
+        def recur(sum_):
+            if sum_ == target:
+                return 1
+            if dp[sum_] == None:
+                res = 0
+                for i in range(size):
+                    if sum_+nums[i]<=target:
+                        val = recur(sum_+nums[i]) 
+                        if val>0:
+                            res += val
+                dp[sum_] = res
+                return dp[sum_]
+            else:
+                return dp[sum_]
+            
+        return recur(0)
+    
     def combinationSum4(self, nums: List[int], target: int) -> int:
         
         size = len(nums)
         count = [0]
-        @functools.lru_cache(None)
+        @lru_cache(None)
         def backtrack(idx,temp):
             if temp==target:
                 #print("yes",temp)
@@ -12,9 +33,11 @@ class Solution:
             res = 0
             for i in range(size):
                 if temp+nums[i] <=target:
+                    #print(temp,nums[i])
                     res+=backtrack(i,temp+nums[i])
+                    #print(res,"here")
             #print(res,"hjk")
             return res
         
         
-        return backtrack(0,0)
+        return self.combinationWithDP(nums,target)
