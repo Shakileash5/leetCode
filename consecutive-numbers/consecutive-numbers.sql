5fbddf34191b6063a1c3071c59ee58622c5a560f
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-07-04 23:27:25 +0530

    Time: 548 ms (18.76%), Space: 0B (100.00%) - LeetHub

--- consecutive-numbers/consecutive-numbers.sql
+++ consecutive-numbers/consecutive-numbers.sql
@@ -4,12 +4,5 @@
 
 #Select Distinct logA.Num as "ConsecutiveNums" from (Select id,Num, LEAD(num,1) OVER(order by id) as num2, LEAD(num,2) OVER(order by id) as num3 from Logs) as logA where logA.Num = logA.num2 and logA.num2 = logA.num3;
 
-select distinct l1.num as 'ConsecutiveNums'
-    from 
-        (select num,row_number() over(order by id) as id from logs) l1 
-        join
-        (select num,row_number() over(order by id) as id from logs) l2 on l2.id=l1.id+1 
-        join
-        (select num,row_number() over(order by id) as id from logs) l3 on l3.id = l1.id+2
-    where  
-          l1.num = l2.num and l3.num = l1.num;
+select distinct l1.num as "ConsecutiveNums" from logs as l1, logs as l2, logs as l3 
+    where l1.id = l2.id+1 and l2.id = l3.id + 1 and l1.num = l2.num and l2.num = l3.num;
