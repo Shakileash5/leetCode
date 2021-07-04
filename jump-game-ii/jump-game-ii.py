768e65010609c37c3ce866f4eccf904e249aedf8
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-07-04 19:58:35 +0530

    Time: 12540 ms (5.01%), Space: 22.8 MB (7.00%) - LeetHub

--- jump-game-ii/jump-game-ii.py
+++ jump-game-ii/jump-game-ii.py
@@ -1,37 +1,48 @@
 class Solution:
-    @cache
     def recur(self,jumpIdx,steps):
-        #print(jumpIdx)
         if jumpIdx >= len(self.nums)-1:
-            #print(steps)
             return 0 
-        #if self.dp[jumpIdx] == None:
-        min_ = 100
-        for i in range(self.nums[jumpIdx],0,-1):
-                #print(jumpIdx,jumpIdx+i)
-            min_ = min(self.recur(jumpIdx+i,steps)+1,min_)
-            #self.dp[jumpIdx]
-        return min_
-        #else:
-        #    return self.dp[jumpIdx]
+        if self.dp[jumpIdx] == None:
+            min_ = 100
+            for i in range(self.nums[jumpIdx],0,-1):
+                min_ = min(self.recur(jumpIdx+i,steps)+1,min_)
+            self.dp[jumpIdx] = min_
+            return min_
+        else:
+            return self.dp[jumpIdx]
             
         return 0
     
     def jump(self, nums: List[int]) -> int:
         
         size = len(nums)
-        """toReach,i = size-1,size-2
-        greedy = [10000 for _ in range(size-1)] + [0]
-        pathCount = 0
-        
-        while i>=0:
-            
-            if nums[i] != 0:
-                greedy[i] = 1+min(greedy[i+1:i+nums[i]+1])
-            i-=1"""
+        def greedy():
+            toReach,i = size-1,size-2
+            greedy = [10000 for _ in range(size-1)] + [0]
+            pathCount = 0
+
+            while i>=0:
+
+                if nums[i] != 0:
+                    greedy[i] = 1+min(greedy[i+1:i+nums[i]+1])
+                i-=1
         
         #return(greedy[0])
         self.nums = nums
         self.dp = [None]*(size+1)
         #print()
-        return self.recur(0,0)
+        def recur(idx):
+            if idx>=size-1:
+                return 0
+            
+            if self.dp[idx] == None:
+                min_ = float('inf')
+                for i in range(nums[idx],0,-1):
+                    min_ = min(min_,recur(idx+i)+1)
+                self.dp[idx] = min_
+                return min_
+            else:
+                return self.dp[idx]
+        
+        #return self.recur(0,0)
+        return recur(0)
