c822954db9166ad0182264c2ac4815047fa7349d
Shakileash C <52775046+Shakileash5@users.noreply.github.com>
2021-07-07 20:18:09 +0530

    Time: 28 ms (91.03%), Space: 14.4 MB (26.28%) - LeetHub

--- longest-palindrome/longest-palindrome.py
+++ longest-palindrome/longest-palindrome.py
@@ -1,23 +1,41 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         size = len(s)
+        
         hashMap = {}
-        oddCount = 0
-        evenCount = 0
+        countUnique = 0
+        totalKey = 0
+        keys = []
         
         for i in range(size):
             if s[i] not in hashMap:
                 hashMap[s[i]] = 1
-                oddCount += 1
+                countUnique += 1
+                totalKey += 1
             else:
-                if hashMap[s[i]]%2 == 0:
-                    oddCount += 1
-                else:
-                    oddCount -= 1
-                
+                if hashMap[s[i]]<=1:
+                    countUnique -= 1
+                    keys.append(s[i])
                 hashMap[s[i]] += 1
+                
+
+        #print(countUnique,totalKey)
+        palindrome = 0
+        flag = 0
+        for key in keys:
+            if hashMap[key]%2 == 1:
+                #print("here")
+                if flag == 0:
+                    flag = 1
+                    palindrome += hashMap[key]
+                else:
+                    palindrome += hashMap[key] - 1
+            else:
+                palindrome += hashMap[key]
+            #print(palindrome,key,hashMap)
+        if flag == 0 and countUnique>0:
+            palindrome += 1
+
+        return palindrome
+            
         
-        #print(oddCount)
-        if oddCount > 0:
-            return size - oddCount + 1
-        return size
