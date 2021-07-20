e569c5d0a546b7002aa9cef491faa48727dd9eb4
Shakileash C <shakileash2000@gmail.com>
2021-07-20 17:42:42 +0530

    Time: 56 ms (87.49%), Space: 14.3 MB (51.18%) - LeetHub

--- combination-sum/combination-sum.py
+++ combination-sum/combination-sum.py
@@ -3,39 +3,17 @@ class Solution:
         size = len(candidates)
         res = []
         
-        def backtrack(i,candidates,res,tem=[]):
-            for j in range(i,size):
-                if sum(tem)<target:
-                    tem.append(candidates[j])
-                    backtrack(j,candidates,res,tem)
-                    if sum(tem) == target:
-                        res.append(tem[:])
-                    tem.pop()
-        
-        def backtrackRewind(idx,temp):
-            if sum(temp)>=target:
-                if sum(temp) == target:
-                    res.append(temp[:])
+        def getCombinations(idx,sum_,temp):
+            if sum_ == target:
+                res.append(temp)
                 return
-            for i in range(idx,size):
-                backtrackRewind(i,temp+[candidates[i]])
+            if idx>=size:
+                return
             
-            return
-        
-        def backtrackSolTwo(idx,sum_,temp):
-            if sum_ == 0:
-                if temp not in res:
-                    res.append(temp)
-                #print(temp)
-                return
             for i in range(idx,size):
-                if sum_ - candidates[i] >= 0:
-                    backtrackSolTwo(i,sum_-candidates[i],temp+[candidates[i]])
+                if sum_+candidates[i]<=target:
+                    getCombinations(i,sum_+candidates[i],temp+[candidates[i]])
             return
         
-        
-        #backtrack(0,candidates,res,[])
-        #backtrackRewind(0,[])
-        backtrackSolTwo(0,target,[])
-        #print(res)
+        getCombinations(0,0,[])
         return res
