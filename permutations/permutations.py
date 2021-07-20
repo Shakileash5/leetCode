398b9b0eade3f3232e310b133420e7a37f6edc0b
Shakileash C <shakileash2000@gmail.com>
2021-07-20 18:35:09 +0530

    Time: 40 ms (71.66%), Space: 14.6 MB (14.75%) - LeetHub

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -1,32 +1,17 @@
 class Solution:
-        def permutations(self,temp,left,right):
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        size = len(nums)
+        res = []
+        def backtrack(left,right,temp):
             if left>=right:
-                self.res.append(temp[:])
+                res.append(temp[:])
                 return
+            
             for i in range(left,right+1):
-                temp[left],temp[i] = temp[i],temp[left]
-                self.permutations(temp,left+1,right)
-                temp[left],temp[i] = temp[i],temp[left]
+                temp[i],temp[left] = temp[left],temp[i]
+                backtrack(left+1,right,temp)
+                temp[i],temp[left] = temp[left],temp[i]
             return
         
-        
-        def permute(self, nums: List[int]) -> List[List[int]]:
-            temp = nums[:]
-            size = len(nums)
-            self.res = []
-
-            def backtrack(temp,l,r):
-                if l>=r:
-                    self.res.append(temp[:])
-                    #print(temp)
-                    #return
-                else:
-                    for i in range(l,r+1):
-                        temp[l],temp[i] = temp[i],temp[l]
-                        backtrack(temp,l+1,r)
-                        temp[l],temp[i] = temp[i],temp[l]
-
-            #backtrack(temp,0,size-1)
-            #print(res)
-            self.permutations(temp,0,size-1)
-            return self.res
+        backtrack(0,size-1,nums[:])
+        return res
