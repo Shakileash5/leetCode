bfab771e48d47d81f5eebecdb530e5a606d98d2a
Shakileash C <shakileash2000@gmail.com>
2021-07-20 18:57:37 +0530

    Time: 40 ms (53.73%), Space: 14.4 MB (56.82%) - LeetHub

--- subsets-ii/subsets-ii.py
+++ subsets-ii/subsets-ii.py
@@ -1,36 +1,17 @@
 class Solution:
-    def backtrack(self,idx,temp,res,nums):
-        if idx>=len(nums):
-            temp.sort()
-            if temp not in res:
-                res.append(temp)
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
+        size = len(nums)
+        res = set()
+        def backtrack(idx,temp):
+            if idx>=size:
+                temp.sort()
+                res.add(tuple(temp))
+                return
+            
+            backtrack(idx+1,temp)
+            backtrack(idx+1,temp+[nums[idx]])
             return
         
-        #self.backtrack(idx+1,temp,res,nums)
-        for i in range(idx,len(nums)):
-            self.backtrack(i+1,temp,res,nums)
-            self.backtrack(i+1,temp+[nums[idx]],res,nums)
-        return
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        size = len(nums)
-        res = []
-        def backtrack(temp,idx,n):
-            if len(temp)==n:
-                temp.sort()
-                if temp not in res:
-                    #print(idxArr)
-                    res.append(temp)
-                    return
-
-            for i in range(idx,size):
-                backtrack(temp+[nums[i]],i+1,n)
-            return 
-        
-        #for i in range(0,size+1):
-        #    backtrack ([],0,i)
-            
-        #backtrack([],0,1)
-        #print(res)
-        #for i in range(0,size+1):
-        self.backtrack(0,[],res,nums)
+        backtrack(0,[])
         return res
