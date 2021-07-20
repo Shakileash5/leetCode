128a032e4bee99031879997dee347788f8f0cb1d
Shakileash C <shakileash2000@gmail.com>
2021-07-20 18:48:48 +0530

    Time: 32 ms (82.28%), Space: 14.5 MB (50.56%) - LeetHub

--- subsets/subsets.py
+++ subsets/subsets.py
@@ -1,29 +1,16 @@
 class Solution:
-    def solutionTwo(self,idx,temp,nums,res):
-        if idx>=len(nums):
-            res.append(temp)
-            return
-        #for i in range(idx,len(nums)):
-        self.solutionTwo(idx+1,temp,nums,res)
-        self.solutionTwo(idx+1,temp+[nums[idx]],nums,res)
-        return
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        size =len(nums)
+        size = len(nums)
+        res = []
         
-        def backtrack(idx,temp,res,n):
-            if len(temp)==n:
-                if temp not in res:
-                    res.append(temp)
-                return
-            for i in range(idx,size):
-                if nums[i] not in temp:
-                    backtrack(i,temp+[nums[i]],res,n)
+        def backtrack(idx,temp):
+            if idx>=size:
+                res.append(temp)
+                return 
+            
+            backtrack(idx+1,temp)
+            backtrack(idx+1,temp+[nums[idx]])
             return
         
-        res = []
-        #for i in range(size+1):
-        #    backtrack(0,[],res,i)
-        #print(res)
-        self.solutionTwo(0,[],nums,res)
+        backtrack(0,[])
         return res
-        
