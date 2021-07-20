3894fe54dc1bb08925e37dd054a63b6ae49363e9
Shakileash C <shakileash2000@gmail.com>
2021-07-20 17:36:50 +0530

    Time: 24 ms (96.46%), Space: 14.5 MB (5.02%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,42 +1,26 @@
-'''def backTrack(res,comd,digit,index,letterFormat):
-        if len(digit) == index:
-            res.append(comd)
-            return
-        else:
-            for i in letterFormat[digit[index]]:
-                comd +=i
-                backTrack(res,comd,digit,index+1,letterFormat)
-                comd = comd[:-1]'''
-    
 class Solution:
-    def backtrackSol2(self,letterFormat,idx,subStr,digits):
-        if idx>=len(digits):
-            if subStr not in self.result:
-                self.result.append(subStr)
+    def letterCombinations(self, digits: str) -> List[str]:
+        size = len(digits)
+        keyPad = {
+            '2':"abc",
+            '3':"def",
+            '4':"ghi",
+            '5':'jkl',
+            '6':'mno',
+            '7':'pqrs',
+            '8':'tuv',
+            '9':'wxyz'
+        }
+        res = []
+        def getCombinations(idx,temp):
+            if idx>=size:
+                if temp!='':
+                    res.append(temp)
+                return
+            
+            for i in range(len(keyPad[digits[idx]])):
+                getCombinations(idx+1,temp+keyPad[digits[idx]][i])
             return
         
-        for i in range(len(letterFormat[digits[idx]])):
-            self.backtrackSol2(letterFormat,idx+1,subStr+letterFormat[digits[idx]][i],digits)
-        return
-    
-    def letterCombinations(self, digits: str) -> List[str]:
-        
-        letterFormat = {
-            "2" : "abc",
-            "3" : "def",
-            "4" : "ghi",
-            "5" : "jkl",
-            "6" : "mno",
-            "7" : "pqrs",
-            "8" : "tuv",
-            "9" : "wxyz"
-        }
-        if digits == "":
-            return []
-        res = []
-        self.result = []
-        #backTrack(res,"",digits,0,letterFormat)
-        self.backtrackSol2(letterFormat,0,"",digits)
-        #print()
-        return self.result
-        
+        getCombinations(0,'')
+        return res
