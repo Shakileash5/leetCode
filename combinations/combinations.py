db43d658e011c173673a83e57628db57f5d54b24
Shakileash C <shakileash2000@gmail.com>
2021-07-20 18:25:05 +0530

    Time: 476 ms (57.76%), Space: 15.8 MB (52.60%) - LeetHub

--- combinations/combinations.py
+++ combinations/combinations.py
@@ -1,29 +1,17 @@
 class Solution:
-    
-    def combinations(self,idx,temp,res,n,k):
-        if len(temp) == k:
-            if temp not in res:
-                res.append(temp)
-            return
-        for i in range(idx,n+1):
-            self.combinations(i+1,temp+[i],res,n,k)
-        return
-    
     def combine(self, n: int, k: int) -> List[List[int]]:
-        
-        def backtrack(idx,temp,res):
-            if len(temp)==k:
-                if temp not in res:
-                    res.append(temp)
-                return
-            for i in range(idx,n+1):
-                if i not in temp:
-                    backtrack(i,temp+[i],res)
-            
-            return 
-        
         res = []
-        #backtrack(1,[],res)
-        self.combinations(1,[],res,n,k)
-        return res
+        def backtrack(num,temp,size):
+            if size == k:
+                res.append(temp)
+                return
+            if num>n:
+                return
+            
+            for i in range(num,n+1):
+                backtrack(i+1,temp+[i],size+1)
+            return
         
+        backtrack(1,[],0)
+        return res
+            
