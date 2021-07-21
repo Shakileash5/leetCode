4df5a9a4ede1b5b6f504f7a07501f24d80e0b704
Shakileash C <shakileash2000@gmail.com>
2021-07-21 12:08:05 +0530

    Time: 4952 ms (60.27%), Space: 14.5 MB (12.41%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,53 +1,31 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        sizeR = len(board)
-        sizeC = len(board[0])
-        
-        sizeW = len(word)
-        
-        def backtrack(i,j,idx,res,visited):
-            
-            if res == word:
-                #print(res)
-                return True
-            if len(res) == len(word) and res!=word:
-                return False
-            #print(visited,"visis")
-            if j+1<sizeC and (i,j+1) not in visited and board[i][j+1] == word[idx]:
-                visited.add((i,j+1))
-                if backtrack(i,j+1,idx+1,res+word[idx],visited):
-                    return True
-                visited.remove((i,j+1))
-            if j-1>=0 and (i,j-1) not in visited and board[i][j-1] == word[idx]:
-                visited.add((i,j-1))
-                if backtrack(i,j-1,idx+1,res+word[idx],visited):
-                    return True
-                visited.remove((i,j-1))
-            if i+1<sizeR and (i+1,j) not in visited and board[i+1][j] == word[idx]:
-                visited.add((i+1,j))
-                if backtrack(i+1,j,idx+1,res+word[idx],visited):
-                    return True
-                visited.remove((i+1,j))
-            if i-1>=0 and (i-1,j) not in visited and board[i-1][j] == word[idx]:
-                visited.add((i-1,j))
-                if backtrack(i-1,j,idx+1,res+word[idx],visited):
-                    return True
-                visited.remove((i-1,j))
-            
-            return False
+        sizeRow = len(board)
+        sizeCol = len(board[0])
+        size = len(word)
         visited = set()
         
-        for i in range(sizeR):
-            for j in range(sizeC):
-                
-                if board[i][j] == word[0]:
-                    #print(i,j)
-                    visited.add((i,j))
-                    res = backtrack(i,j,1,word[0],visited)
-                    visited.remove((i,j))
-                    #print("aftee",res)
-                    if res == True:
-                        return True
+        def backtrack(idx,idxR,idxC):
+            if idx>=size:
+                return True
+            visited.add((idxR,idxC))
+            #print(idxR,idxC,idx)
+            for i,j in [(0,1),(1,0),(-1,0),(0,-1)]:
+                x = i+idxR
+                y = j+idxC
+                if 0<=x<sizeRow and 0<=y<sizeCol and (x,y) not in visited:
+                    #print(x,y)
+                    if word[idx] == board[x][y]:
+                        if backtrack(idx+1,x,y):
+                            return True
+            visited.remove((idxR,idxC))
+            return False
         
-        #print(backtrack(0,0,1,"A",visited))
+        for i in range(sizeRow):
+            for j in range(sizeCol):
+                if board[i][j] == word[0]:
+                    visited = set()
+                    if backtrack(1,i,j):
+                        return True
         return False
+        
