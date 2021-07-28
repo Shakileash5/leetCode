39e12b2af11f52b39a0a85b9d37d8dca8c6373da
Shakileash C <shakileash2000@gmail.com>
2021-07-28 12:10:58 +0530

    Time: 812 ms (5.90%), Space: 32.4 MB (9.08%) - LeetHub

--- palindromic-substrings/palindromic-substrings.py
+++ palindromic-substrings/palindromic-substrings.py
@@ -1,63 +1,47 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        
         size = len(s)
-        count = 0
-        hashMap = {}
+        memo = [[None for j in range(size+2)] for i in range(size+2)]
         
-        def isPalindrome(string):
-            size = len(string)
+        def isPalindrome(str_):
+            sizePali = len(str_)
             left = 0
-            right = size - 1
+            right = sizePali - 1
+            
             while(left<right):
-                if string[left] != string[right]:
+                if str_[left] != str_[right]:
                     return False
-                left+=1
-                right-=1
+                left += 1
+                right -= 1
+            
             return True
         
-        def bruteForce():
+        def isPali(s,left,right):
+            if left+1 == right:
+                return s[left] == s[right]
+            
+            if left == right:
+                return True
+            
+            if left>right:
+                return False
+            
+            if memo[left][right] == None:
+                memo[left][right] = False
+                if s[left] == s[right]:
+                    memo[left][right] = isPali(s,left+1,right-1)
+                    
+            return memo[left][right]
+        
+        #print(isPali("AhbhA",0,4))
+        def naive():
+            count = 0
             for i in range(size):
-                for j in range(i,size):
-                    subStr = s[i:j+1]
-                    subSize = len(subStr)
-                    #print(subStr)
-                    flag = 0
-                    if subSize%2==0:
-                        if hashMap.get(subStr[1:subSize-1],0):
-                            flag = 1
-                            if subStr[0] == subStr[subSize-1]:
-                                count+=1
-                    elif subSize%2==1 and subSize>=3:
-                        if hashMap.get(subStr[1:subSize-1],0):
-                            flag = 1
-                            if subStr[0] == subStr[subSize-1]:
-                                count+=1
-                    if flag ==0 and isPalindrome(subStr):
-                            hashMap[subStr] = 1
-                            count+=1
-        if s is None or s.strip() == '':
-            return 0
-        
-        totalPal = 0
-        
-        for i in range (0,len(s)):
-            
-            # for odd len pal, single char at center 
-            totalPal += self.countPal(s,i,i)
-            
-            # for even Len pal, consecutive char at center
-            totalPal += self.countPal(s,i,i+1)
-            
-        return totalPal
-        
-    def countPal(self,s,lo,hi):
-        count = 0
-        while lo >= 0 and hi < len(s) and s[lo] == s[hi]:
-            lo -= 1
-            hi += 1
-            count +=1   # each occurance is one count 
-        #print("Count is : ", count)    
-        return count # s[lo+1,hi]
-                
-            
+                #print(i)
+                for j in range(i+1,size+1):
+                    #print(s[i:j])
+                    if isPali(s,i,j-1):
+                        count += 1
+            return count
+        #print(naive())
+        return naive()
