ee23dbda08b813ff5548de987ea7f87c919fb7f0
Shakileash C <shakileash2000@gmail.com>
2021-07-30 08:33:00 +0530

    Time: 559 ms (16.20%), Space: 0B (100.00%) - LeetHub

--- customers-who-never-order/customers-who-never-order.sql
+++ customers-who-never-order/customers-who-never-order.sql
@@ -2,3 +2,4 @@
 #Select  Customers.Name as Customers from Customers Left Join Orders on Customers.id = Orders.CustomerId where Orders.CustomerId is Null; 
 
 select name as "Customers" from Customers where id not in (select customerId from orders);
+#select C1.name as "Customers" from Customers C1 left join orders C2 on C1.id = C2.CustomerId ;
