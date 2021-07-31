2a79b5fb5ea31d5d8866623efaeedb043a7c0eac
Shakileash C <shakileash2000@gmail.com>
2021-07-31 22:29:50 +0530

    Time: 860 ms (31.00%), Space: 18.4 MB (6.97%) - LeetHub

--- 01-matrix/01-matrix.py
+++ 01-matrix/01-matrix.py
@@ -1,67 +1,27 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        """"m = len(mat)
-        n = len(mat[0])
-        
-        ans = [[0 for i in range(n)] for j in range(m)]
-        dirs = [(0,1),(0,-1),(1,0),(-1,0)]
+        sizeRow = len(mat)
+        sizeCol = len(mat[0])
         
         visited = set()
+        queue = []
         
-        def bfs(queue):
-            distance = 0
-            queueUtils = []
-            while queue:
-                temp = queue.pop(0)
-                for a, b in dirs:
-                    if -1 < a + temp[0] < m and -1 < b + temp[1] < n:
-                        if (temp[0]+a, temp[1]+b) in visited:
-                            distance += 1
-                            return distance
-                        elif mat[temp[0]+a][temp[1]+b] == 0:
-                            distance += 1
-                            return distance
-                        else:
-                            queueUtils.append((temp[0]+a, temp[1]+b))
-                if len(queue) == 0:
-                    distance += 1
-                    queue = queueUtils[:]
-                    queueUtils = []
-            return distance
-        
-        
-        for i in range(m):
-            for j in range(n):
-                if mat[i][j] == 1:
-                    queue = []
-                    queue.append((i, j))
-                    distance = bfs(queue)
-                    ans[i][j] = distance
-                else:
-                    visited.add((i, j))
-        
-        return ans"""
-        n,m = len(mat), len(mat[0])
-        
-        ans = [[-1 if mat[i][j]!=0 else 0 for j in range(m)] for i in range(n)]
-        dirxn = [(1,0),(-1,0),
-                (0,1),(0,-1)]
-        
-        stack = collections.deque()
-        seen = set()
-        for i in range(n):
-            for j in range(m):
+        for i in range(sizeRow):
+            for j in range(sizeCol):
                 if mat[i][j] == 0:
-                    seen.add((i,j))
-                    stack.append((i,j,0))
+                    visited.add((i,j))
+                    queue.append((i,j,0))
         
-        while stack:
-            i,j,depth = stack.popleft()
-            ans[i][j] = depth
-            for dx, dy in dirxn:
-                x,y = i+dx, j+dy
-                if 0<=x<n and 0<=y<m and (x,y) not in seen:
-                    seen.add((x,y))
-                    stack.append((x,y,depth+1))
-        return ans
-    
+        res = [[0 for i in range(sizeCol)] for j in range(sizeRow)]
+        #print(mat)
+        #print(res)
+        while queue:
+            node = queue.pop(0)
+            #print(node)
+            res[node[0]][node[1]] = node[2]
+            
+            for x,y in [(node[0]+1,node[1]),(node[0]-1,node[1]),(node[0],node[1]+1),(node[0],node[1]-1)]:
+                if 0<=x<sizeRow and 0<=y<sizeCol and (x,y) not in visited:
+                    visited.add((x,y))
+                    queue.append((x,y,node[2]+1))
+        return res
