b7a90cb122086e3d409468929fb66c1a661b6076
Shakileash C <shakileash2000@gmail.com>
2021-08-01 22:03:49 +0530

    Time: 5380 ms (5.02%), Space: 402 MB (5.49%) - LeetHub

--- coin-change/coin-change.py
+++ coin-change/coin-change.py
@@ -2,60 +2,36 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
         size = len(coins)
-        res = [100000]
-        resCount = 0
-        idx = size-1
-        #coins.sort()
-        #print(coins)
-        @cache
-        def recur(idx,amountTemp,count):
+        coins.sort(reverse=True)
+        memo = {}
+        
+        def minCoins(idx,sum_):
             if idx>=size:
-                return
-            if amountTemp<=0:
-                if amountTemp == 0:
-                    #print(amountTemp,idx,count)
-                    res[0] = min(res[0],count)
-                return
-            
-            for i in range(idx,size):
-                recur(i,amountTemp-coins[i],count+1)
-                if (i+1)<size:
-                    recur(i+1,amountTemp-coins[i+1],count+1)
-        
-        #recur(0,amount,0)
-        
-        def dpSol(size,amount):
-            dp = [0] + [inf] * amount
-            for coin in coins:
-                for i in range(coin, amount+1):
-                    dp[i] = min(dp[i], dp[i-coin]+1)
-            return dp[-1] if dp[-1] < inf else -1
-        
-        #print(resCount,res,amountTemp)
-        
-        def recur(idx,amount,memo):
-            if idx>=size:
-                print(amount)
-                if ammount == 0:
+                if sum_ == 0:
                     return 0
                 return float('inf')
-            if amount == 0:
-                return 0
+            if sum_ <= 0:
+                if sum_ == 0:
+                    return 0
+                return float('inf')
             
-            if (idx,amount) not in memo:
-                minCoin = float('inf')
+            
+            if (idx,sum_) not in memo:
+                min_ = float('inf')
                 for i in range(idx,size):
-                    #print("here",i,amount)
-                    if (amount - coins[i])>=0:
-                        #print("inside")
-                        res = recur(i,amount - coins[i],memo) + 1
-                        #print(amount,idx,res)
-                        minCoin = min(minCoin,res)
-                memo[(idx,amount)] = minCoin
-                return memo[(idx,amount)]
-            else:
-                return memo[(idx,amount)]
-        #return dpSol(size,amount)
-        memo = {}
-        res  = recur(0,amount,memo)
-        return -1 if res >= float('inf') else res
+                    #print(i,sum_)
+                    if sum_-coins[i]>=0:
+                        #print(i)
+                        res = minCoins(i,sum_-coins[i])+1
+                        min_ = min(min_,res) 
+                    
+                memo[(idx,sum_)] = min_
+            
+            return memo[(idx,sum_)]
+        
+        res = minCoins(0,amount)
+        if res == float('inf'):
+            return -1
+        return res
+                
+                
