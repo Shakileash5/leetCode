bf4a0ccb7415ecec605feb4000edbc1417e46b45
Shakileash C <shakileash2000@gmail.com>
2021-08-01 23:47:09 +0530

    Time: 696 ms (58.11%), Space: 15.6 MB (17.24%) - LeetHub

--- shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -3,27 +3,25 @@ class Solution:
         sizeRow = len(grid)
         sizeCol = len(grid[0])
         
-        minDist = float('inf')
-        visited = set()
-        visited.add((0,0))
-        queue = [[0,0,1]]
-        flag = 0
         if grid[0][0] == 1:
             return -1
+        
+        queue = [(0,0,1)]
+        visited = set()
+        visited.add((0,0))
+        distance = -1
+        
         while queue:
-            i,j,dist = queue.pop(0)
-            
+            i,j,depth = queue.pop(0)
+            #print(queue,visited,i,j,depth)
             if i == sizeRow-1 and j == sizeCol-1:
-                minDist = min(minDist,dist)
-                flag = 1
-            #print(i,j,dist,grid[i][j])
-            for x,y in [(i+1,j),(i-1,j),(i,j+1),(i,j-1),(i-1,j-1),(i+1,j+1),(i-1,j+1),(i+1,j-1)]:
-                if 0<=x<sizeRow and 0<=y<sizeCol and grid[x][y]!=1 and (x,y) not in visited:
-                    visited.add((x,y))
-                    queue.append([x,y,dist+1])
-                    
-        return minDist if flag == 1 else -1 
-                
+                distance = depth
+                break
             
+            for x,y in [(i+1,j),(i-1,j),(i,j+1),(i,j-1),(i+1,j+1),(i-1,j-1),(i+1,j-1),(i-1,j+1)]:
+                if 0<=x<sizeRow and 0<=y<sizeCol and grid[x][y] == 0:
+                    if (x,y) not in visited:
+                        visited.add((x,y))
+                        queue.append((x,y,depth+1))
         
-        
+        return distance
