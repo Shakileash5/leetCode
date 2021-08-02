17d1e6f8144d5d5d7f87f5c8ce3d1c4a3dc0eed3
Shakileash C <shakileash2000@gmail.com>
2021-08-02 23:12:01 +0530

    Time: 36 ms (95.40%), Space: 14.8 MB (63.73%) - LeetHub

--- search-a-2d-matrix/search-a-2d-matrix.py
+++ search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,57 +1,32 @@
 class Solution:
-    def rowMajor(self,idx,sizeR,sizeC):
-        row = idx//sizeC
-        col = idx%sizeC
-        if sizeR == 1:
-            return 0,idx
-        return row,col
-    
-    def binarySearch(self,left,right,target,matrix):
-        if left>right:
-            return False
-        
-        mid = (left+right)//2
-        row,col = self.rowMajor(mid,len(matrix),len(matrix[0]))
-        #print(row,col,mid)
-        if matrix[row][col] == target:
-            return True
-        if matrix[row][col]<target:
-            return self.binarySearch(mid+1,right,target,matrix)
-        else :
-            return self.binarySearch(left,mid-1,target,matrix)
-        return
-    
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        sizeR = len(matrix)
-        sizeC = len(matrix[0])
-        #print(sizeC)
-        def rowMajor(x):
-            i = x//sizeC 
-            j = x%sizeC 
+        sizeRow = len(matrix)
+        sizeCol = len(matrix[0])
+        
+        def rowMajor(idx):
+            i = idx // sizeCol
+            j = idx % sizeCol
+            if sizeRow == 1:
+                return 0,idx
             
-            if sizeR == 1:
-                return 0,x
             return i,j
         
+        #print(rowMajor(3))
+        left = 0
+        right = sizeRow*sizeCol-1
         
-        """def binarySearch(target,left,right):
-            
+        while(left<=right):
             mid = (left+right)//2
+            
             i,j = rowMajor(mid)
-            print(left,right,mid,i,j)
-            
-            if left>right:
-                return False
-            
+            #print(i,j,left,right,mid)
             if matrix[i][j] == target:
                 return True
-            
-            
-            if matrix[i][j] < target:
-                return binarySearch(target,mid+1,right)
+            if matrix[i][j]>target:
+                right = mid - 1
             else:
-                return binarySearch(target,left,mid-1)
-        """
-        #return binarySearch(target,0,sizeR*sizeC-1)
-        return self.binarySearch(0,sizeR*sizeC-1,target,matrix)
-        #print(self.rowMajor(0,1,2))
+                left = mid + 1
+                
+        return False
+        
+        
