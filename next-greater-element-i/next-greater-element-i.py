a27a3474a123f91978f1dd1949e956a2d8f4b0bf
Shakileash C <shakileash2000@gmail.com>
2021-08-03 22:56:08 +0530

    Time: 52 ms (48.58%), Space: 14.7 MB (14.07%) - LeetHub

--- next-greater-element-i/next-greater-element-i.py
+++ next-greater-element-i/next-greater-element-i.py
@@ -3,21 +3,22 @@ class Solution:
         size1 = len(nums1)
         size2 = len(nums2)
         
-        stack  = []
+        stack = []
         hashMap = {}
         res = []
         
         for i in range(size2-1,-1,-1):
-            
-            while stack and stack[-1]<=nums2[i]:
-                ele = stack.pop()
-                
-            hashMap[nums2[i]] = [i,stack[-1] if stack else -1]
+            while stack and stack[-1]<nums2[i]:
+                stack.pop()
+            if stack:
+                hashMap[nums2[i]] = stack[-1]
+            else:
+                hashMap[nums2[i]] = -1 
             stack.append(nums2[i])
-            
-        print(stack)
+        
         for i in range(size1):
-            res.append(hashMap[nums1[i]][1])    
-            
-            
+            res.append(hashMap[nums1[i]])
+        
         return res
+            
+            
