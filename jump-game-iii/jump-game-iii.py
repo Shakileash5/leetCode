66a82163d9b38ce3cf98688ef42e2378b9e1b330
Shakileash C <shakileash2000@gmail.com>
2021-08-04 18:11:45 +0530

    Time: 312 ms (35.87%), Space: 77.4 MB (15.81%) - LeetHub

--- jump-game-iii/jump-game-iii.py
+++ jump-game-iii/jump-game-iii.py
@@ -1,21 +1,20 @@
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
         size = len(arr)
-        visited = [False for i in range(size+1)]
-        def recur(idx):
-            
+        
+        visited = [False for i in range(size)]
+        def dfs(idx):
             if arr[idx] == 0:
                 return True
-            if visited[idx]:
-                return False
+        
             visited[idx] = True
-            if 0 <= idx + arr[idx] < size:
-                if recur(idx+arr[idx]):
+            if arr[idx]+idx<=size-1 and visited[arr[idx]+idx]==False:
+                if dfs(arr[idx]+idx):
                     return True
-            if 0<= idx-arr[idx] < size:
-                if recur(idx-arr[idx]):
+            if idx-arr[idx]>=0 and visited[idx-arr[idx]]==False:
+                if dfs(idx-arr[idx]):
                     return True
             
             return False
         
-        return recur(start)
+        return dfs(start)
