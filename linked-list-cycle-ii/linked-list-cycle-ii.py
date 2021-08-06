6fede12bc9954d4e3d77fbdf1895aa79e66d0cd7
Shakileash C <shakileash2000@gmail.com>
2021-08-06 22:34:44 +0530

    Time: 48 ms (81.20%), Space: 17.3 MB (55.80%) - LeetHub

--- linked-list-cycle-ii/linked-list-cycle-ii.py
+++ linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -5,44 +5,12 @@
 #         self.next = None
 
 class Solution:
-    def getPosition(self,head):
-        tempHead = head
-        count = 0
-        visited = set()
-        slow = head
-        fast = head
-        while slow and fast:
-            if slow in visited:
-                return slow
-            visited.add(slow)
-            slow = slow.next
-            if fast.next:
-                fast = fast.next.next
-            else:
-                return None
-            
-            
-        
-        
-            
-            
-        
-        
     def detectCycle(self, head: ListNode) -> ListNode:
-        fastPointer = head
-        slowPointer = head
-        visitedSet = set()
-        """
-        while(slowPointer!=None and fastPointer!=None):
-            
-            if slowPointer in visitedSet:
-                #print(slowPointer.val)
-                return slowPointer
-            visitedSet.add(slowPointer)
-            slowPointer = slowPointer.next
-            
-            if fastPointer.next == None:
-                return None
-            fastPointer = fastPointer.next.next
-            """
-        return self.getPosition(head)
+        hashMap = {}
+        
+        while(head):
+            if head in hashMap:
+                return head
+            hashMap[head] = 1
+            head = head.next
+        return None
