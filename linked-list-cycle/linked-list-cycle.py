3761d8575c094f8294805c713ce52da8edd84006
Shakileash C <shakileash2000@gmail.com>
2021-08-06 22:28:03 +0530

    Time: 56 ms (58.29%), Space: 17.6 MB (80.87%) - LeetHub

--- linked-list-cycle/linked-list-cycle.py
+++ linked-list-cycle/linked-list-cycle.py
@@ -5,37 +5,15 @@
 #         self.next = None
 
 class Solution:
-    def findCycle(self,head):
+    def hasCycle(self, head: ListNode) -> bool:
+        
         slow = head
         fast = head
         
-        while slow and fast:
+        while fast and fast.next:
             slow = slow.next
-            if fast.next:
-                fast = fast.next.next
-            else:
-                return False
+            fast = fast.next.next
             if slow == fast:
                 return True
-            
+        
         return False
-        
-    
-    def hasCycle(self, head: ListNode) -> bool:
-        
-        def solOne():
-            fastPointer = head
-            slowPointer = head
-
-            while(slowPointer!=None and fastPointer!=None):
-
-                slowPointer = slowPointer.next
-                if fastPointer.next == None:
-                    return False
-                fastPointer = fastPointer.next.next
-
-                if slowPointer == fastPointer:
-                    return True
-            return False
-        
-        return self.findCycle(head)
