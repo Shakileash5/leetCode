7fdd6ca91654c7390555887208a647bc25a9fa10
Shakileash C <shakileash2000@gmail.com>
2021-08-06 19:00:34 +0530

    Time: 40 ms (33.43%), Space: 15.4 MB (99.47%) - LeetHub

--- reverse-linked-list/reverse-linked-list.py
+++ reverse-linked-list/reverse-linked-list.py
@@ -4,30 +4,15 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def recur(self,head,parent,revHead):
-        if head == None:
-            revHead = parent
-            return revHead
-        revHead = self.recur(head.next,head,revHead)
-        head.next = parent
-        return revHead
-    
     def reverseList(self, head: ListNode) -> ListNode:
-        def reverseIterative():
-            prev = None
-            current =  head
-            if current == None:
-                return None
-            next = head.next
-
-            while(current!=None):
-                next = current.next
-                current.next = prev
-                prev = current
-                current = next
-
-            return prev
+        prev = None
+        curr = head
+        next_ = None
         
-        return self.recur(head,None,None)
+        while(curr):
+            next_ = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_
         
-        
+        return prev
