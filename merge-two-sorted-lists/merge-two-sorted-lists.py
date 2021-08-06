cffd3d23849e3e90fea37cd77591952a28f8ab86
Shakileash C <shakileash2000@gmail.com>
2021-08-06 18:33:17 +0530

    Time: 32 ms (92.05%), Space: 14.2 MB (62.67%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.py
+++ merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -4,96 +4,53 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def solutionTwo(self,l1,l2):
-        if l1==None and l2==None:
-            return None
-        if l1 == None:
-            return l2
-        if l2 == None:
-            return l1
-        resultNode = ListNode()
-        res = resultNode
-        if l1.val<l2.val:
-            resultNode.val = l1.val
-            l1 = l1.next
-        else:
-            resultNode.val = l2.val
-            l2 = l2.next
-        
-        while l1 and l2:
-            if l1.val>=l2.val:
-                node = ListNode(l2.val)
-                resultNode.next = node
-                resultNode = resultNode.next
-                l2 = l2.next
-            else:
-                node = ListNode(l1.val)
-                resultNode.next = node
-                resultNode = resultNode.next
-                l1 = l1.next
-        if l1 and l2 == None:
-            resultNode.next = l1
-        elif l2 and l1 == None:
-            resultNode.next = l2
-        
-        #print(res)
-        return res
-            
-            
-        
-        
-        
-        
-        
-        
-    
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        def solutionOne():
-            index1 = l1
-            index2 = l2
-
-            if l1 == None and l2 != None:
-                return l2
-            if l2 == None and l1 != None:
-                return l1
-            if l1 == None and l2 == None:
-                return l1
-
-            if l1.val > l2.val:
-                l1,l2 = l2,l1
-                index1,index2 = index2,index1
-
-            resHead = ListNode()
-            res = resHead
-
-            while l1!=None and l2!=None:
-
-                    if l1.val<l2.val:
-                        res.val = l1.val
-                        res.next = ListNode()
-                        res = res.next
-                        l1 = l1.next
-                    elif l2.val<l1.val:
-                        res.val = l2.val
-                        res.next = ListNode()
-                        res = res.next
-                        l2 = l2.next
-                    else:
-                        res.val = l1.val
-                        res.next = ListNode()
-                        res = res.next
-                        l1 = l1.next 
-
-
-
-            if l1 == None and l2!=None:
-                res.val = l2.val
-                res.next = l2.next
-            elif l2 == None and l1!=None:
-                res.val = l1.val
-                res.next = l1.next
-
-            #print(resHead,"\n",l1,"\n",l2)   
-        #return resHead
         
-        return self.solutionTwo(l1,l2)         
+        if l1 == None and l2 == None:
+            return None
+        if l2 == None and l1 != None:
+            return l1
+        if l1 == None and l2 != None:
+            return l2
+        
+        if l2.val<l1.val:
+            l1,l2 = l2,l1
+        
+        start1 = l1
+        start2 = l2
+        
+        res = ListNode()
+        resOr = res
+        prev = None
+        
+        while start1 and start2:
+            if start1.val<start2.val:
+                res.val = start1.val
+                start1 = start1.next
+            else:
+                res.val = start2.val
+                start2 = start2.next
+            prev = res
+            res.next = ListNode()
+            res = res.next
+        
+        while start1:
+            res.val = start1.val
+            res.next = ListNode()
+            prev = res
+            res = res.next
+            start1 = start1.next
+        
+        while start2:
+            res.val = start2.val
+            res.next = ListNode()
+            prev = res
+            res = res.next
+            start2 = start2.next
+        prev.next = None
+        return resOr
+        
+                
+            
+                
+        
