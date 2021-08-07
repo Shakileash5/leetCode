d9689648cd1d92e0fd8b5a0763100c800885214f
Shakileash C <shakileash2000@gmail.com>
2021-08-07 17:59:26 +0530

    Time: 72 ms (89.65%), Space: 18.2 MB (78.56%) - LeetHub

--- lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -4,40 +4,18 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
- 
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        def isChildPresent(rootNode,child):
-            if rootNode == None:
-                return 
-            if rootNode == child:
-                return True
+        def helper(root,p,q):
+            if root  == None:
+                return None
             
-            if isChildPresent(rootNode.left,child) == True:
-                return True
-            if isChildPresent(rootNode.right,child) == True:
-                return True
+            if root.val<p.val and root.val<q.val:
+                return helper(root.right,p,q)
+            elif root.val>p.val and root.val>q.val:
+                return helper(root.left,p,q)
+            return root
         
-        def LCA(rootNode):
-            if rootNode == None:
-                return 
-            
-                #return True
-            
-            if LCA(rootNode.left) == True:
-                return True
-            
-            if LCA(rootNode.right) == True:
-                return True
-            
-            #print(rootNode.val,"val")
-            if isChildPresent(rootNode,p) and isChildPresent(rootNode,q):
-                #print(rootNode.val)
-                LCA.node = rootNode
-                return True
-        
-        LCA.node = None
-        LCA(root)
-        #print(isChildPresent(root.left,q))    
-        return LCA.node
+        return helper(root,p,q)
