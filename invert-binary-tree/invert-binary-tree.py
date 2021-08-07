cd3cdc10dfd4e65594c22663bbab133c75a8659f
Shakileash C <shakileash2000@gmail.com>
2021-08-07 17:49:18 +0530

    Time: 32 ms (63.63%), Space: 14.4 MB (11.84%) - LeetHub

--- invert-binary-tree/invert-binary-tree.py
+++ invert-binary-tree/invert-binary-tree.py
@@ -5,47 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def invertTreeTwo(self,leftSubtree,rightSubtree,root):
-        if root == None:
-            return
-        if leftSubtree==None and rightSubtree==None:
-            return None
-        root.left = rightSubtree
-        root.right = leftSubtree
-        if leftSubtree:
-            self.invertTreeTwo(leftSubtree.left,leftSubtree.right,leftSubtree)
-        if rightSubtree:
-            self.invertTreeTwo(rightSubtree.left,rightSubtree.right,rightSubtree)
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         
-        return
-        
-    
-    def invertTree(self, root: TreeNode) -> TreeNode:
-        
-        if root == None:
-            return None
-        
-        #if root.left and root.right:
-        leftSubtree = root.left
-        rightSubtree = root.right
-        
-        def recur(root1,root2,parent):
-            if root1 == None and root2 == None:
-                return
+        def invertHelper(root):
+            if root == None:
+                return None
             
-            #root1.val,root2.val = root2.val,root1.val
-            parent.left = root2
-            parent.right = root1
-            if root1:
-                recur(root1.left,root1.right,root1)
-            if root2:
-                recur(root2.left,root2.right,root2)
-            #print(root1.val,root2.val)
-            
-            return
+            left = invertHelper(root.left)
+            right = invertHelper(root.right)
+            root.left = right
+            root.right = left
+            return root
         
-        #recur(leftSubtree,rightSubtree,root)
-        #print(leftSubtree)
-        self.invertTreeTwo(leftSubtree,rightSubtree,root)
-        return root
-        
+        return invertHelper(root)
