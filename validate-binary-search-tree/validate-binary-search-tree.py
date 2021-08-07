68abad67290613d7d7ef6600b8f12e96901b2a21
Shakileash C <shakileash2000@gmail.com>
2021-08-07 17:23:09 +0530

    Time: 44 ms (70.68%), Space: 17.2 MB (13.69%) - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.py
+++ validate-binary-search-tree/validate-binary-search-tree.py
@@ -5,43 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
-    def inorder(self,root,temp):
-        if root == None:
-            return True
-        if self.inorder(root.left,temp) == False:
-            return False
-        if temp==[] or temp[-1]<root.val:
-            temp.append(root.val)
-        else:
-            return False
-        if self.inorder(root.right,temp) == False:
-            return False
-        return True
-    
-    def isValidBST(self, root: TreeNode) -> bool:
-        
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
         res = []
-        def recur(root):
-            
+        
+        def inorder(root):
             if root == None:
                 return True
             
-            v = recur(root.left)
-            if v == False:
+            if inorder(root.left) == False:
                 return False
-            if res==[] or res[-1] < root.val:
+            if res == [] or res[-1]<root.val:
                 res.append(root.val)
             else:
                 return False
-            u = recur(root.right)
-            if u == False:
+            if inorder(root.right) == False:
                 return False
+            return True
         
-        #result = recur(root)
-        #if result == None:
-        #    return True
-        #else:
-        #    return False
-        return self.inorder(root,[])    
-        
+        return inorder(root)
+            
