42f4dbec83d6eecb89d7297c60fdbe36f5dc3e09
Shakileash C <shakileash2000@gmail.com>
2021-08-07 10:50:19 +0530

    Time: 7848 ms (5.00%), Space: 17.5 MB (51.14%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,46 +1,35 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
         size = len(nums)
-        res = []
         nums.sort()
+        hashMap = {}
+        res = set()
         
-        def binarySearch(arr,left,right,target):
-            if left>right:
-                return False
-            mid = (left+right)//2
+        def binarySearch(left,right,target):
             
-            if arr[mid] == target:
-                return True
-            if arr[mid]>target:
-                return binarySearch(arr,left,mid-1,target)
-            else:
-                return binarySearch(arr,mid+1,right,target)
+            while(left<=right):
+                mid = (left+right)//2
+                
+                if nums[mid] == target:
+                    return mid
+                if nums[mid]>target:
+                    right = mid-1
+                else:
+                    left = mid+1
+            return -1
+        
         #print(nums)
         for i in range(size):
-            if i!=0  and nums[i]==nums[i-1]:
-                continue 
-            left = i+1
-            right = size-1
-            while left<right:
-                #print(nums[i],nums[left],nums[right])
-                toFind = nums[right] + nums[i] + nums[left]
-                if toFind<0:
-                    left+=1
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+            
+            for j in range(i+1,size):
+                target = 0 - (nums[i]+nums[j])
+                idx = binarySearch(j+1,size-1,target)
+                if idx == -1:
                     continue
-                elif toFind>0:
-                    right-=1
-                    continue
-                
-                pairs = [nums[i],nums[left],nums[right]]
-                #print(pairs)
-                if pairs not in res:
-                    res.append(pairs)
-                while left<right and nums[left] == pairs[1]:
-                    left+=1
-                while left<right and nums[right] == pairs[2]:
-                    right-=1
-                
-                
-                
+                #print(idx,i,j,target)
+                #if [nums[i],nums[j],nums[idx]] not in res:
+                res.add((nums[i],nums[j],nums[idx]))
         return res
+                
