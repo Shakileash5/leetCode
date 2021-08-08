5d1bfd50c610d9ab62b38554db0447529ce7657c
Shakileash C <shakileash2000@gmail.com>
2021-08-08 11:49:38 +0530

    Time: 64 ms (31.21%), Space: 19 MB (37.00%) - LeetHub

--- balanced-binary-tree/balanced-binary-tree.py
+++ balanced-binary-tree/balanced-binary-tree.py
@@ -5,72 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isBalancedTwo(self,root):
-        def height(root):
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        def getHeight(root):
             if root == None:
                 return 0
             
-            return max(height(root.left),height(root.right)) + 1
+            return max(getHeight(root.left),getHeight(root.right))+1
         
-        def recur(root):
+        #print(getHeight(root))
+        
+        def isBalancedHelper(root):
             if root == None:
                 return True
             
-            heightL = height(root.left)
-            heightR = height(root.right)
-            
-            if abs(heightL-heightR)>1:
+            lHeight = getHeight(root.left)
+            rHeight = getHeight(root.right)
+            if abs(lHeight-rHeight)>1:
                 return False
-            
-            if recur(root.left) == False:
+            if isBalancedHelper(root.left) == False:
                 return False
-            if recur(root.right) == False:
+            if isBalancedHelper(root.right) == False:
                 return False
-            
-            return True
-            
-        #print()
-        return recur(root)
-        
-        
-    def isBalanced(self, root: TreeNode) -> bool:
-        
-        if root == None:
             return True
         
-        def height(root,hh,high):
-            if root == None:
-                return None
-            if high[0]<hh:
-                high[0] = hh
+        return isBalancedHelper(root)
             
-            height(root.left,hh+1,high)
-            height(root.right,hh+1,high)
-            
-            return high[0]
-        
-        def recur(root,leftHigh,rightHigh):
-            
-            if root == None:
-                return
-            
-            high = [0]
-            lh = 0
-            if root.left:
-                lh = height(root.left,1,high)
-            high = [0]
-            rh = 0
-            if root.right:
-                rh = height(root.right,1,high)
-            #print(root.val,lh,rh,leftHigh,rightHigh)
-            if abs(lh-rh)>1:
-                return False
-            if recur(root.left,leftHigh+1,rightHigh) == False:
-                return False
-            if recur(root.right,leftHigh,rightHigh+1) == False:
-                return False
-            
-            return True
-        
-        #return recur(root,1,1)
-        return self.isBalancedTwo(root)
