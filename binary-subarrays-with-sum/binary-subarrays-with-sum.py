e200ae41e6344709afe7d92c21b2a7dcd13d7fb8
Shakileash C <shakileash2000@gmail.com>
2021-08-09 00:08:45 +0530

    Time: 280 ms (92.53%), Space: 17.7 MB (42.32%) - LeetHub

--- binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,16 +1,14 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        size = len(nums)        
-        count = 0
         hashMap = {}
         hashMap[0] = 1
         sum_ = 0
-        for i in range(size):
-            sum_ += nums[i]
-            if sum_-goal in hashMap:
+        count = 0
+        
+        for val in nums:
+            sum_ += val
+            if sum_ - goal in hashMap:
                 count += hashMap[sum_-goal]
-            hashMap[sum_] = hashMap.get(sum_,0) + 1
-            #print(count,hashMap,sum_)
+            hashMap[sum_] = hashMap.get(sum_,0)+1
         
         return count
-                
