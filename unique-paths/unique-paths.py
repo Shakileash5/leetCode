b7d1d2c6dafec47f1d677d986166ef2476f85107
Shakileash C <shakileash2000@gmail.com>
2021-08-09 23:06:57 +0530

    Time: 28 ms (86.61%), Space: 14.1 MB (86.86%) - LeetHub

--- unique-paths/unique-paths.py
+++ unique-paths/unique-paths.py
@@ -1,37 +1,19 @@
 class Solution:
-    
-    def findPath(self,idxR,idxC,m,n):
-        if (idxR>=m) or (idxC>=n):
-            #print(idxR,idxC)
-            #print("why god")
-            return 0
-        if idxR==(m-1) and idxC==(n-1) :
-            #print("foundIt")
-            return 1
-        
-        if self.dp[idxR][idxC] == None:
-            pathTwo = self.findPath(idxR,idxC+1,m,n) 
-            #print("comeDown",idxR,idxC)
-            path = self.findPath(idxR+1,idxC,m,n) 
-            self.dp[idxR][idxC] = pathTwo+path
-            return self.dp[idxR][idxC]
-        else:
-            return self.dp[idxR][idxC]    
-            #return self.findPath(idxR,idxC+1,m,n) + self.findPath(idxR+1,idxC,m,n)
-        
-    
-    
     def uniquePaths(self, m: int, n: int) -> int:
-        paths = [[1 for i in range(n)] for j in range(m)]
         
-        #print(paths)
-        self.dp = [[None for i in range(n)] for j in range(m)]
-        """for i in range(1,m):
-            for j in range(1,n):
-                #print(i,j,i-1,j-1)
-                paths[i][j] = paths[i-1][j] + paths[i][j-1]"""
+        dp = [[None for j in range(n+1)] for i in range(m+1)]
         
-        #print(paths)
-        #print()
-        #return paths[-1][-1]
-        return self.findPath(0,0,m,n)
+        def dfs(idxR,idxC):
+            if idxR >= m-1 or idxC >= n-1:
+                return 1
+            
+            if dp[idxR][idxC] == None:
+                res = 0
+                if idxR+1<m:
+                    res += dfs(idxR+1,idxC)
+                if idxC+1<n:
+                    res += dfs(idxR,idxC+1)
+                dp[idxR][idxC] = res
+            return dp[idxR][idxC]
+        
+        return dfs(0,0)
