95907219b9114f6bb1bfd583c6b7e77f4e9f35d9
Shakileash C <shakileash2000@gmail.com>
2021-08-09 23:13:13 +0530

    Time: 36 ms (94.37%), Space: 14.5 MB (33.71%) - LeetHub

--- unique-paths-ii/unique-paths-ii.py
+++ unique-paths-ii/unique-paths-ii.py
@@ -1,67 +1,28 @@
 class Solution:
-    def recur(self,grid,idxR,idxC,m,n):
-        if idxR>=n or idxC>=m:
-            return 0
-        if grid[idxR][idxC] == 1:
-            return 0
-        if idxR==(n-1) and idxC==(m-1):
-            return 1
-        if self.memoize[idxR][idxC] == None:
-            path = self.recur(grid,idxR+1,idxC,m,n)
-            pathTwo = self.recur(grid,idxR,idxC+1,m,n)
-            self.memoize[idxR][idxC] = path+pathTwo
-            return self.memoize[idxR][idxC]
-        else:
-            return self.memoize[idxR][idxC]
-        
-    
-    
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        n = len(obstacleGrid)
-        m = len(obstacleGrid[0])
-        """flag = 0
-        print(n,m)
-        res = [[0 for j in range(m)] for i in range(n)]
         
-        if obstacleGrid[0][0] == 0:
-            res[0][0] = 1
+        sizeRow = len(obstacleGrid)
+        sizeCol = len(obstacleGrid[0])
         
-        for j in range(1,m):
-            if obstacleGrid[0][j] == 0:
-                res[0][j] = res[0][j-1]
-        for i in range(1,n):
-            if obstacleGrid[i][0] == 0:
-                res[i][0] = res[i-1][0]
-                
+        dp = [[None for j in range(sizeCol+1)] for i in range(sizeRow)]
         
-        print(res)
-        for i in range(1,n):
-            for j in range(1,m):
-                if obstacleGrid[i][j] == 0:
-                    res[i][j] = res[i-1][j] + res[i][j-1]"""
+        def dfs(idxR,idxC):
+            #print(idxR,idxC)
+            if idxR>=sizeRow or idxC>=sizeCol:
+                return 0
+            
+            if obstacleGrid[idxR][idxC] == 1:
+                #print(idxR,idxC,"d")
+                return 0
+            
+            if idxR==sizeRow-1 and idxC==sizeCol-1:
+                return 1
+            
+            if dp[idxR][idxC] == None:
+                res = 0
+                res += dfs(idxR+1,idxC)
+                res += dfs(idxR,idxC+1)
+                dp[idxR][idxC] = res
+            return dp[idxR][idxC]
         
-        path = [[0 for i in range(m)] for j in range(n)]
-        if obstacleGrid[0][0] == 0:
-            path[0][0] = 1
-        for i in range(1,m):
-            if obstacleGrid[0][i] == 1:
-                break
-            path[0][i] = path[0][i-1]
-        for i in range(1,n):
-            if obstacleGrid[i][0] == 1:
-                break
-            path[i][0] = path[i-1][0]
-        
-        for i in range(1,n):
-            for j in range(1,m):
-                if obstacleGrid[i][j] != 1:
-                    path[i][j] = path[i][j-1] + path[i-1][j]
-        
-        #print(res)
-        #print(path)
-        #return path[-1][-1]
-        self.memoize = [[None for i in range(m)] for j in range(n)]
-        if obstacleGrid[0][0] == 1 or obstacleGrid[-1][-1] == 1:
-            return 0
-        return self.recur(obstacleGrid,0,0,m,n)       
-        
+        return dfs(0,0)
