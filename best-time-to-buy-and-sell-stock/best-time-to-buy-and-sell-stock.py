0677a007787dff9fa5d7d0aed067373b5d3e8035
Shakileash C <shakileash2000@gmail.com>
2021-08-10 07:26:08 +0530

    Time: 1004 ms (68.62%), Space: 25.1 MB (52.68%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,56 +1,13 @@
 class Solution:
-    def localMaxMin(self,prices):
-        maxProfit = 0
-        buy = prices[0]
+    def maxProfit(self, prices: List[int]) -> int:
         size = len(prices)
+        max_ = 0
+        buy = prices[0]
         
         for i in range(1,size):
-            if buy>prices[i]:
+            if prices[i]<buy:
                 buy = prices[i]
             else:
-                maxProfit = max(maxProfit,prices[i]-buy)
-        #print(maxProfit)
-        return maxProfit
+                max_ = max(max_,prices[i]-buy)
         
-    def maxProfit(self, prices: List[int]) -> int:
-        def recur(arr,idx,val,maxVal):
-            if arr == []:
-                return
-            if len(arr) == 1:
-                if val>arr[0]:
-                    return -1
-                else:
-                    return arr[0]
-                
-            if arr[idx]>maxVal[0] and val<arr[idx]:
-                maxVal[0] = arr[idx]
-                
-            if idx == len(arr)-1:
-                #print("here",val,maxVal,arr)
-                return maxVal[0]
-            
-            #print("iteration",idx)
-            recur(arr,idx+1,val,maxVal)
-            return maxVal[0]
-        
-        def solutionOne():
-            size = len(prices)
-            minVal = prices[0]
-            maxProfit = -1
-            for i in range(size):
-                minVal = min(prices[i],minVal)
-                tempProfit = prices[i] - minVal
-                maxProfit = max(maxProfit,tempProfit)
-            return maxProfit
-        
-        size = len(prices)
-        min_ = prices[0]
-        max_ =  0
-        for i in range(1,size):
-            if min_> prices[i]:
-                min_ = prices[i]
-            elif max_<(prices[i]-min_):
-                max_ = prices[i] - min_
-        #print(max_)
-        #return self.localMaxMin(prices)
         return max_
