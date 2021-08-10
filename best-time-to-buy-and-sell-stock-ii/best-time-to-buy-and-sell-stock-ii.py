2ebcf9ef73067df54f6676b09d3d61dfcadf123d
Shakileash C <shakileash2000@gmail.com>
2021-08-10 09:10:35 +0530

    Time: 64 ms (42.69%), Space: 15 MB (65.19%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,22 +1,10 @@
 class Solution:
-    def localMaxMin(self,prices):
-        maxProfit = 0
-        sell = prices[-1]
+    def maxProfit(self, prices: List[int]) -> int:
         size = len(prices)
+        maxProfit = 0
+        
         for i in range(size-1,0,-1):
             if prices[i]>prices[i-1]:
                 maxProfit += prices[i]-prices[i-1]
-        return maxProfit
-    
-    def maxProfit(self, prices: List[int]) -> int:
         
-        """maxSum = 0
-        size = len(prices)
-        for i in range(size-1,0,-1):
-            if prices[i]>prices[i-1]:
-                maxSum += prices[i]-prices[i-1]
-            
-        return maxSum"""
-        return self.localMaxMin(prices)
-            
-            
+        return maxProfit
