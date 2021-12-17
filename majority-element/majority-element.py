0c2bb6327b00ff6b749da3c6a7116432521af1ed
Shakileash C <shakileash2000@gmail.com>
2021-12-17 18:03:57 +0530

    Time: 193 ms (23.71%), Space: 15.6 MB (11.32%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,17 +1,29 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        rejected = {}
-        
+    def majorityElementTwo(self,nums):
+        hashMap = {}
         size = len(nums)
+        threshold = size//2
         
         for i in range(size):
-            if nums[i] not in rejected.keys():
-                count = nums.count(nums[i])
-                if count>(size//2):
-                    return nums[i]
-                else:
-                    rejected[nums[i]] = True
-                
-        
+            hashMap[nums[i]] = hashMap.get(nums[i],0)+1
+            if hashMap[nums[i]]>threshold:
+                return nums[i]
         return 0
+    
+    def majorityElement(self, nums: List[int]) -> int:
+        hashMap = {}
+        size = len(nums)
+        res = (None,0)
+        
+        for i in range(size):
+            if nums[i] not in hashMap:
+                hashMap[nums[i]] = 1
+                if res[1] < 1:
+                    res = (nums[i],1)
+            else:
+                hashMap[nums[i]] += 1
+                if res[1] < hashMap[nums[i]]:
+                    res = (nums[i],hashMap[nums[i]])
+        
+        return res[0]
                     
