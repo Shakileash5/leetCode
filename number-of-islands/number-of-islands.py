6257ced6d34c943456fa20f33f22e35fc1c6bb2c
Shakileash C <shakileash2000@gmail.com>
2021-12-17 23:07:33 +0530

    Time: 284 ms (92.79%), Space: 25.8 MB (5.17%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,34 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
         sizeR = len(grid)
         sizeC = len(grid[0])
-        visited = [[0 for _ in range(sizeC)] for i in range(sizeR)]
+        visited = set()
         noOfIsland = 0
         
-        def searchIsland(idxR,idxC):
-            if grid[idxR][idxC] == "0":
+        def dfs(idxR,idxC):
+            if (idxR,idxC) in visited:
                 return
-            visited[idxR][idxC] = 1
-            if idxR+1<sizeR and visited[idxR+1][idxC]==0 :
-                searchIsland(idxR+1,idxC)
-            if idxC+1<sizeC and visited[idxR][idxC+1]==0:
-                searchIsland(idxR,idxC+1)
-            if idxR-1>=0 and visited[idxR-1][idxC]==0:
-                searchIsland(idxR-1,idxC)
-            if idxC-1>=0 and visited[idxR][idxC-1]==0:
-                searchIsland(idxR,idxC-1)
+            
+            visited.add((idxR,idxC))
+            if(idxR+1 < sizeR) and grid[idxR+1][idxC] == '1':
+                dfs(idxR+1,idxC)
+            if(idxR-1 >= 0) and grid[idxR-1][idxC] == '1':
+                dfs(idxR-1,idxC)
+            if(idxC+1 < sizeC) and grid[idxR][idxC+1] == '1':
+                dfs(idxR,idxC+1)
+            if(idxC-1 >= 0) and grid[idxR][idxC-1] == '1':
+                dfs(idxR,idxC-1)
+            
             return
         
         for i in range(sizeR):
             for j in range(sizeC):
-                if grid[i][j]=="1" and visited[i][j] == 0:
-                    searchIsland(i,j)
-                    noOfIsland+=1
-                    
-        #searchIsland(0,0)
-        print(noOfIsland)
-        return noOfIsland
-        
-        
-                
+                if(grid[i][j] == '1'):
+                    if (i,j) not in visited:
+                        dfs(i,j)
+                        noOfIsland += 1
+        return noOfIsland 
+            
