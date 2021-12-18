86f01c0edb9a02479aaff42035d3679424f9099f
Shakileash C <shakileash2000@gmail.com>
2021-12-18 22:51:33 +0530

    Time: 6036 ms (10.86%), Space: 33.4 MB (68.75%) - LeetHub

--- count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
+++ count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.py
@@ -1,14 +1,53 @@
-import bisect
 class Solution:
+    
+    def getInsertPos(self,arr,val):
+        if arr == []:
+            return 0
+        left = 0
+        right = len(arr) - 1
+        
+        while(left+1<right):
+            mid = (left+right)//2
+            if arr[mid] == val:
+                right = mid
+            if val > arr[mid]:
+                left = mid
+            else:
+                right = mid
+        if arr[right]<val:
+            return right + 1
+        if arr[left]<val:
+            return left + 1
+        return 0
+    
     def countSmaller(self, nums: List[int]) -> List[int]:
-        size = len(nums)
         res = []
         stack = []
-        for i in range(size-1,-1,-1):
-            idx = bisect.bisect_left(stack,nums[i])
-            res.append(idx)
-            stack.insert(idx,nums[i])
+        size = len(nums)
+        def getInsertPos(arr,target):
+            if arr == []:
+                return 0
             
-        #print(stack,res)
-        return res[::-1]
+            left = 0
+            right = len(arr)-1
+            while(left+1<right):
+                mid = (left+right)//2
+                if arr[mid] == target:
+                    right = mid
+                if arr[mid]<target:
+                    left = mid
+                else:
+                    right = mid
+            
+            if arr[right]<target:
+                return right + 1
+            if arr[left]<target:
+                return left + 1
+            return 0
         
+        for i in range(size-1,-1,-1):
+            pos = self.getInsertPos(stack,nums[i])
+            res.append(pos)
+            stack.insert(pos,nums[i])
+            #print(nums[i],pos,stack)
+        return res[::-1]
