e388d041b1f45fdb7449332fc3720d3122fdccdf
Shakileash C <shakileash2000@gmail.com>
2021-12-18 19:46:30 +0530

    Time: 2366 ms (30.47%), Space: 17.6 MB (50.33%) - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -1,35 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        res = set()
         size = len(nums)
         nums.sort()
-        hashMap = {}
-        res = set()
         
-        def binarySearch(left,right,target):
-            
-            while(left<=right):
-                mid = (left+right)//2
-                
-                if nums[mid] == target:
-                    return mid
-                if nums[mid]>target:
-                    right = mid-1
-                else:
-                    left = mid+1
-            return -1
-        
-        #print(nums)
         for i in range(size):
-            if i>0 and nums[i] == nums[i-1]:
-                continue
-            
-            for j in range(i+1,size):
-                target = 0 - (nums[i]+nums[j])
-                idx = binarySearch(j+1,size-1,target)
-                if idx == -1:
-                    continue
-                #print(idx,i,j,target)
-                #if [nums[i],nums[j],nums[idx]] not in res:
-                res.add((nums[i],nums[j],nums[idx]))
-        return res
+            left = i + 1
+            right = size - 1
+            while(left<right):
+                tempArr = (nums[i],nums[left],nums[right])
+                tempSum = sum(tempArr)
                 
+                if tempSum < 0 :
+                    left += 1
+                    continue
+                if tempSum > 0 : 
+                    right -= 1;
+                    continue
+                
+                if tempSum == 0:
+                    res.add(tempArr)
+                
+                while left<right and nums[left] == tempArr[1]:
+                    left += 1
+                
+                while left<right and nums[right] == tempArr[2]:
+                    right -= 1
+        
+        return res
+                    
