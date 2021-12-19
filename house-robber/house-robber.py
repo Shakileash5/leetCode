a081a416fe82e8a17707455ed52d673b6991df55
Shakileash C <shakileash2000@gmail.com>
2021-12-19 14:44:24 +0530

    Time: 32 ms (65.28%), Space: 14.1 MB (93.37%) - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -1,57 +1,16 @@
 class Solution:
-    def robTwo(self,nums):
-        size = len(nums)
-        dp = [None]*(size+1)
+    
+    def recur(self,nums,idx,dp):
+        if idx >= len(nums):
+            return 0
         
-        def robber(idx):
-            if idx>=size:
-                return 0
-            
-            if dp[idx] == None:
-                dontRob = robber(idx+1)
-                rob = robber(idx+2) + nums[idx]
-                dp[idx] = max(dontRob,rob)
-                return dp[idx]
-            else:
-                return dp[idx]
+        if dp[idx] == None:
+            dontRob = self.recur(nums,idx+1,dp)
+            robHouse = self.recur(nums,idx+2,dp) + nums[idx]
+            dp[idx] = max(dontRob,robHouse)
         
-        return robber(0)
+        return dp[idx]
     
     def rob(self, nums: List[int]) -> int:
-        
-        def solutionOne():
-            size = len(nums)
-            dp = [None]*(size+3)
-            if size == 1:
-                return nums[0]
-            #@LruCache
-            def recur(temp,idx):
-                if idx>=size:
-                    sum = 0
-                    #print(temp)
-                    for i in temp:
-                        if isinstance(i,int):
-                            sum+=i
-                        else:
-                            if len(i)!=0:
-                                sum+=i[0]
-                    #print(sum,dp)
-                    recur.maxi = max(recur.maxi,sum)
-
-                    return recur.maxi
-                dp[idx+2] =  recur(temp+[nums[idx+2:idx+3]],idx+2)
-                dp[idx+3] =  recur(temp+[nums[idx+3:idx+4]],idx+3)
-
-            recur.maxi = -1
-            #recur([nums[0]],0)
-            #recur([nums[1]],1)
-
-            res = nums.copy()
-            res.extend([0,0,0])
-            for i in range(size-1,-1,-1):
-                res[i] = res[i]+max(res[i+2],res[i+3])
-
-            #print(house1,house2)
-            return max(res[0],res[1])
-        
-        return self.robTwo(nums)
+        dp = [None]*(len(nums))
+        return self.recur(nums,0,dp)
