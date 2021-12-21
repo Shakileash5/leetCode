0f7984b5b1c5d38516d9ef205119b18a5b5ec17f
Shakileash C <shakileash2000@gmail.com>
2021-12-21 23:07:15 +0530

    Time: 100 ms (58.03%), Space: 16.7 MB (49.56%) - LeetHub

--- course-schedule/course-schedule.py
+++ course-schedule/course-schedule.py
@@ -46,6 +46,27 @@ class Graph:
                 if self.isCycleUtil(key,stack,visited):
                     return True
         return False
+    
+    def dfs(self,node,visited):
+        if node not in self.nodes:
+            return False
+        visited.add(node)
+        for key in self.nodes[node]:
+            if key not in visited:
+                if not self.dfs(key,visited):
+                    return False
+                if key == node:
+                    return True
+        return True
+            
+    def is_cycle(self):
+        visited = set() # to keep track of visited nodes
+        for key in self.nodes.keys():
+            if key not in visited:
+                if not self.dfs(key,visited):
+                    return False
+        return True
+    
         
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
